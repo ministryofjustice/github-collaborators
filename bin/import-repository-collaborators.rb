226b13272b2a64d5eb51b0e053c544a5268c0c4c
David Salgado <david@digitalronin.com>
2020-11-24 13:40:25 +0900

    Rename script
    We will use this to import existing repository
    collaborators, not just to create the terraform
    code

--- bin/import-repository-collaborators.rb
+++ bin/import-repository-collaborators.rb
@@ -5,10 +5,8 @@ require_relative "../lib/github_collaborators"
 
 def main(repository)
   collaborators = external_collaborators(repository)
-  terraform = render_template(repository, collaborators)
-  output_file = "terraform/#{repository}.tf"
-  File.write(output_file, terraform)
-  puts "Generated terraform file: #{output_file}"
+  create_terraform_file(repository, collaborators)
+  import_collaborators(repository, collaborators)
 end
 
 def external_collaborators(repository)
@@ -16,6 +14,19 @@ def external_collaborators(repository)
     github_token: ENV.fetch("ADMIN_GITHUB_TOKEN"),
     login: "ministryofjustice"
   ).for_repository(repository)
+end
+
+def create_terraform_file(repository, collaborators)
+  terraform = render_template(repository, collaborators)
+  output_file = "terraform/#{repository}.tf"
+  File.write(output_file, terraform)
+  puts "Generated terraform file: #{output_file}"
+end
+
+def import_collaborators(repository, collaborators)
+# terraform init
+# terraform import module.acronyms.github_repository_collaborator.collaborator[\"matthewtansini\"] acronyms:matthewtansini
+
 end
 
 def render_template(repository, collaborators)
@@ -37,5 +48,3 @@ end
 repository = ARGV.shift
 raise "USAGE: #{$0} [repository name]" if repository.nil?
 main(repository)
-
-
