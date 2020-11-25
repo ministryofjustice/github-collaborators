c37a3bf536d9769537f59e52502cceacb7e1ed6f
David Salgado <david@digitalronin.com>
2020-11-25 13:51:56 +0900

    Run a targeted terraform plan
    A full plan takes several minutes, which is a long
    time to keep users waiting for feedback on their
    PRs. This change uses a shell script to run a 
    targeted terraform plan against each module (i.e.
    repository) which is changed by the current PR.
    
    NB: This will break if the terraform/*.tf filename
    does not match the module name of the repository
    collaborators.

---
+++ scripts/targeted-plan.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script to run a targeted plan against only those repositories' terraform
+# files which are changed by the current PR. A full `terraform plan` takes
+# several minutes, so this should enable people to get faster feedback on
+# proposed changes.
+#
+# NB: This is quite brittle, because it depends on the module name exactly
+# matching the `terraform/*.tf` filename
+
+set -euo pipefail
+
+main() {
+  cd terraform
+  echo "Running terraform init"
+  # Suppress init output, unless it fails
+  output=$(${TERRAFORM} init -input=false 2>&1) || (echo "$output" && false)
+
+  for r in $(changed_repositories); do
+    echo "Running terraform plan for ${r}"
+    ${TERRAFORM} plan -target=module.${r}
+  done
+}
+
+changed_repositories() {
+  git diff main --name-only | sed 's/^terraform.\(.*\)\.tf$/\1/'
+}
+
+main
