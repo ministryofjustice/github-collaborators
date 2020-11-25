69964784d4f6f1225b0b15a6f8737631ca002f6e
David Salgado <david@digitalronin.com>
2020-11-25 06:35:31 +0000

    Replace toonsend w. DangerDawson
    Another change to test github actions

--- terraform/testing-external-collaborators.tf
+++ terraform/testing-external-collaborators.tf
@@ -2,6 +2,6 @@ module "testing-external-collaborators" {
   source     = "./modules/repository-collaborators"
   repository = "testing-external-collaborators"
   collaborators = {
-    toonsend     = "triage"
+    DangerDawson = "push"
   }
 }
