fa40eabd6cf045cd49210af0e57d35cd256057a7
David Salgado <david@digitalronin.com>
2020-11-25 06:28:06 +0000

    Remove DangerDawson from testing-external-collabs
    This is to test the terraform apply action, when this PR is merged

--- terraform/testing-external-collaborators.tf
+++ terraform/testing-external-collaborators.tf
@@ -2,7 +2,6 @@ module "testing-external-collaborators" {
   source     = "./modules/repository-collaborators"
   repository = "testing-external-collaborators"
   collaborators = {
-    DangerDawson = "push"
     toonsend     = "triage"
   }
 }
