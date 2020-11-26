b0ec721033be2cf1f70208f773c4b1832756343f
David Salgado <david@digitalronin.com>
2020-11-26 14:11:03 +0900

    WiP: Use a richer data object for collaborators
    I want the code to document additional metadata
    about the collaboration

--- terraform/modules/repository-collaborators/variables.tf
+++ terraform/modules/repository-collaborators/variables.tf
@@ -4,7 +4,27 @@ variable "repository" {
 }
 
 variable "collaborators" {
-  type        = map
-  description = "github_user:permission map for repository external collaborators"
-  default     = {}
+  description = "Details of all the external collaborators of a repository"
+  type = list(object({
+    github_user  = string
+    permission   = string
+    name         = string
+    email        = string
+    org          = string
+    reason       = string
+    added_by     = string
+    review_after = string
+  }))
+  default = [
+    {
+      github_user  = ""
+      permission   = ""
+      name         = ""
+      email        = ""
+      org          = ""
+      reason       = ""
+      added_by     = ""
+      review_after = ""
+    }
+  ]
 }
--- terraform/testing-external-collaborators.tf
+++ terraform/testing-external-collaborators.tf
@@ -1,7 +1,18 @@
 module "testing-external-collaborators" {
   source     = "./modules/repository-collaborators"
   repository = "testing-external-collaborators"
-  collaborators = {
-    DangerDawson = "push"
-  }
+  collaborators = [
+    {
+      github_user = "DangerDawson"
+      permission  = "push"
+      name        = "David Dawson"
+      email       = "not@real.email"
+    },
+    {
+      github_user = "toonsend"
+      permission  = "triage"
+      name        = "David Townsend"
+      email       = "not@real.email"
+    }
+  ]
 }
