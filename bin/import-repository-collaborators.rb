445be8ec9751a3911dedc394e7a45c108ed734f5
David Salgado <david@digitalronin.com>
2020-11-27 17:12:47 +0900

    Remove unused parameter

--- bin/import-repository-collaborators.rb
+++ bin/import-repository-collaborators.rb
@@ -9,5 +9,4 @@ raise "USAGE: #{$0} [repo name1] [repo name2] ..." if repositories.empty?
 GithubCollaborators::RepositoryCollaboratorImporter.new(
   terraform_dir: "terraform",
   terraform_executable: ENV.fetch("TERRAFORM"),
-  github_token: ENV.fetch("ADMIN_GITHUB_TOKEN"),
 ).import(repositories)
