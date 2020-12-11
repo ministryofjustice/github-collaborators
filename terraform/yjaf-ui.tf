6e126401f16484ee91d257c0e2a0ccdeab7c860d
oliviergaubert <64133325+oliviergaubert@users.noreply.github.com>
2020-12-11 11:30:30 +0100

    Update yjaf-ui.tf
    Need access to YJAF UI Nginx config

--- terraform/yjaf-ui.tf
+++ terraform/yjaf-ui.tf
@@ -52,5 +52,15 @@ module "yjaf-ui" {
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
       review_after = "2021-12-11"
     },
+       {
+      github_user  = "oliviergaubert"
+      permission   = "admin"
+      name         = "Olivier Gaubert"
+      email        = "Olivier.Gaubert@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
+    },
   ]
 }
