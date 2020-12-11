a86d6396cefe5ae78373a05a57c9e63916810234
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2020-12-11 09:29:48 +0000

    Add meta data to Northgate ext collab github YJAF users

--- terraform/yjaf-devops-infrastructure.tf
+++ terraform/yjaf-devops-infrastructure.tf
@@ -5,62 +5,72 @@ module "yjaf-devops-infrastructure" {
     {
       github_user  = "brbaje-dev"
       permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Ben Bajek"
+      email        = "ben.bajek@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
     },
     {
       github_user  = "chris-nps"
       permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Chris Sweeney"
+      email        = "chris.sweeney@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
     },
     {
       github_user  = "oliviergaubert"
-      permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      permission   = "admin"
+      name         = "Olivier Gaubert"
+      email        = "Olivier.Gaubert@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
     },
     {
       github_user  = "gregi2n"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Greg Whiting"
+      email        = "greg.whiting@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
     },
     {
       github_user  = "griffinjuknps"
       permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Jeremy Griffin"
+      email        = "jeremy.griffin@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
     },
     {
       github_user  = "AndrewRichards72"
       permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Andrew Richards"
+      email        = "andrew.richards@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
+    },
+    {
+      github_user  = "TomDover-NorthgatePS"
+      permission   = "push"
+      name         = "Tom Dover"
+      email        = "tom.dover@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
     },
   ]
 }
