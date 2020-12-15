0c2eebc9950a5664c1004a3a28035e3b850c1b56
Neil Kidd <neil.kidd@madetech.com>
2020-12-15 17:41:24 +0000

    Updated all details except

--- terraform/staff-device-dns-dhcp-infrastructure.tf
+++ terraform/staff-device-dns-dhcp-infrastructure.tf
@@ -5,72 +5,72 @@ module "staff-device-dns-dhcp-infrastructure" {
     {
       github_user  = "Themitchell"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "Andy Mitchell" #  The name of the person behind github_user
+      email        = "andrew.mitchell@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "thip"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "David Capper" #  The name of the person behind github_user
+      email        = "david.capper@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "neilkidd"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "Neil Kidd" #  The name of the person behind github_user
+      email        = "neil.kidd@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "jivdhaliwal"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "Jiv Dhaliwal" #  The name of the person behind github_user
+      email        = "jiv.dhaliwal@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "CaitBarnard"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "Caitlin Barnard" #  The name of the person behind github_user
+      email        = "caitlin@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "jbevan4"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "Joshua-Luke Bevan" #  The name of the person behind github_user
+      email        = "joshua.bevan@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "efuaakum"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
+      name         = "Efua Akumanyi" #  The name of the person behind github_user
+      email        = "efua.akumanyi@madetech.com" #  Their email address
+      org          = "Made Tech Ltd" #  The organisation/entity they belong to
+      reason       = "Working on PTTP Security Logging and DNS / DHCP services" #  Why is this person being granted access?
       added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2021-03-01" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
 }
