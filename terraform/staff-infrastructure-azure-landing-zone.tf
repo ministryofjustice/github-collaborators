1ff90e47a57dedbe4fb8fd40443c0fa2a339bfc8
MatthewWhiteMoJ <61458526+MatthewWhiteMoJ@users.noreply.github.com>
2020-12-17 13:47:03 +0000

    Add sol-tec collaborators

--- terraform/staff-infrastructure-azure-landing-zone.tf
+++ terraform/staff-infrastructure-azure-landing-zone.tf
@@ -5,22 +5,42 @@ module "staff-infrastructure-azure-landing-zone" {
     {
       github_user  = "bsharpen-soltec"
       permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Barry Sharpen" #  The name of the person behind github_user
+      email        = "bsharpen@sol-tec.com" #  Their email address
+      org          = "Sol-Tec" #  The organisation/entity they belong to
+      reason       = "PTTP Tech team building Azure Landing Zone" #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "cscales-soltec"
       permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Chris Scales" #  The name of the person behind github_user
+      email        = "cscales@sol-tec.com" #  Their email address
+      org          = "Sol-Tec" #  The organisation/entity they belong to
+      reason       = "PTTP Tech team building Azure Landing Zone" #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+        {
+      github_user  = "RyanLWilliams"
+      permission   = "push"
+      name         = "Ryan Williams" #  The name of the person behind github_user
+      email        = "rwilliams@sol-tec.com" #  Their email address
+      org          = "Sol-Tec" #  The organisation/entity they belong to
+      reason       = "PTTP Tech team building Azure Landing Zone" #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+        {
+      github_user  = "jabenny"
+      permission   = "push"
+      name         = "Josh Benfield" #  The name of the person behind github_user
+      email        = "jbenfield@sol-tec.com" #  Their email address
+      org          = "Sol-Tec" #  The organisation/entity they belong to
+      reason       = "PTTP Tech team building Azure Landing Zone" #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
 }
