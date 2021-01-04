3de78369747924a80c6403611edff57df69e433b
David Capper <david.capper@madetech.com>
2021-01-04 13:42:19 +0000

    Add Made Tech Collaborators to PTTP IMA repos

--- terraform/staff-infrastructure-metric-aggregation-server.tf
+++ terraform/staff-infrastructure-metric-aggregation-server.tf
@@ -4,43 +4,23 @@ module "staff-infrastructure-metric-aggregation-server" {
   collaborators = [
     {
       github_user  = "thip"
-      permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      permission   = "admin"
+      name         = "David Capper"
+      email        = "david.capper@madetech.com"
+      org          = "Made Tech Ltd"
+      reason       = "PTTP Tech Team"
+      added_by     = "richard.baguley@justice.gov.uk"
+      review_after = "2021-06-01"
     },
     {
-      github_user  = "elena-vi"
-      permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
-      github_user  = "chubberlisk"
-      permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
-      github_user  = "jbevan4"
-      permission   = "push"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
-      reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      github_user  = "elcorbs"
+      permission   = "admin"
+      name         = "Emma Corbett" 
+      email        = "emma@madetech.com" 
+      org          = "Made Tech Ltd"
+      reason       = "PTTP Tech Team"
+      added_by     = "richard.baguley@justice.gov.uk"
+      review_after = "2021-06-01"
     },
   ]
 }
