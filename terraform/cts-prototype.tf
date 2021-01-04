3ef2fc4bdb23b92592c3af8f39aab7782bb6abc0
Javid <javid.ali@digital.justice.gov.uk>
2021-01-04 12:22:52 +0000

    Update cts-prototype.tf

--- terraform/cts-prototype.tf
+++ terraform/cts-prototype.tf
@@ -3,13 +3,13 @@ module "cts-prototype" {
   repository = "cts-prototype"
   collaborators = [
     {
-      github_user  = "jkosem"
+      github_user  = "digitalali-moj"
       permission   = "admin"
-      name         = "" #  The name of the person behind github_user
-      email        = "" #  Their email address
-      org          = "" #  The organisation/entity they belong to
+      name         = "Javid Ali" #  The name of the person behind github_user
+      email        = "javid.ali@digital.justice.gov.uk" #  Their email address
+      org          = "Ministry of Justice" #  The organisation/entity they belong to
       reason       = "" #  Why is this person being granted access?
-      added_by     = "" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      added_by     = "Staff Tools & Services <staffservices@digital.justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "" #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
