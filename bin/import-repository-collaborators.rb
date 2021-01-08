fe9ad7c1e87271847ede1a0304338fdd0b378120
Jake Benilov <benilov@gmail.com>
2021-01-08 12:39:51 +0000

    Fix broken 'bin/import-repository-collaborators.rb` script
    Add in missing comma

--- bin/import-repository-collaborators.rb
+++ bin/import-repository-collaborators.rb
@@ -8,6 +8,6 @@ raise "USAGE: #{$0} [repo name1] [repo name2] ..." if repositories.empty?
 
 GithubCollaborators::RepositoryCollaboratorImporter.new(
   terraform_dir: "terraform",
-  terraform_executable: ENV.fetch("TERRAFORM")
+  terraform_executable: ENV.fetch("TERRAFORM"),
   org_ext_collabs: OrganizationExternalCollaborators.new(login: "ministryofjustice"),
 ).import(repositories)
