e49cd9ca8048948dd063fc1d4e5bc65a4ee0f4c4
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2021-01-11 11:52:47 +0000

    Add yjaf-cmm Northgate collaborators

--- terraform/yjaf-cmm.tf
+++ terraform/yjaf-cmm.tf
@@ -4,9 +4,19 @@ module "yjaf-cmm" {
   collaborators = [
     {
       github_user  = "gregi2n"
-      permission   = "push"
+      permission   = "admin"
       name         = "Greg Whiting"
       email        = "greg.whiting@northgateps.com"
+      org          = "Northgate"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      review_after = "2021-12-11"
+    },
+    {
+      github_user  = "TomDover-NorthgatePS"
+      permission   = "push"
+      name         = "Tom Dover"
+      email        = "tom.dover@northgateps.com"
       org          = "Northgate"
       reason       = "Part of the Northgate supplier team for the YJB YJAF system"
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
