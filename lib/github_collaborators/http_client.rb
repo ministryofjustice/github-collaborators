b048ee568340d097345be860b58ffdf414725ef8
David Salgado <david@digitalronin.com>
2021-01-15 16:00:50 +0900

    Add code to remove collaborators
    This code can
    
    * raise an issue in the repository, explaining why 
    the collaborator has been removed, and what to do 
    about it. 
    
    * remove the collaborator's access to the 
    repository.

---
+++ lib/github_collaborators/http_client.rb
@@ -0,0 +1,39 @@
+class GithubCollaborators
+  class HttpClient
+    attr_reader :token
+
+    def initialize(params = {})
+      token = params.fetch(:token, ENV.fetch("ADMIN_GITHUB_TOKEN"))
+    end
+
+    def post_json(url, json)
+      http, uri = client(url)
+      request = Net::HTTP::Post.new(uri.request_uri, headers)
+      request.body = json
+      http.request(request)
+    end
+
+    def delete(url)
+      http, uri = client(url)
+      request = Net::HTTP::Delete.new(uri.request_uri, headers)
+      http.request(request)
+    end
+
+    private
+
+    def client(url)
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      [http, uri]
+    end
+
+    def headers
+      {
+        "Content-Type" => "application/json",
+        "Authorization" => "token #{token}",
+      }
+    end
+  end
+end
+
---
+++ spec/issue_creator_spec.rb
@@ -0,0 +1,25 @@
+class GithubCollaborators
+  describe IssueCreator do
+    let(:params) { {
+      owner: "ministryofjustice",
+      repository: "somerepo",
+      github_user: "somegithubuser",
+    } }
+
+    subject(:ic) { described_class.new(params) }
+
+    let(:http_client) { double(HttpClient) }
+
+    let(:json) {
+      %[{"title":"Please define external collaborators in code","assignees":["somegithubuser"],"body":"Hi there\\n\\nWe now have a process to manage github collaborators in code:\\nhttps://github.com/ministryofjustice/github-collaborators/blob/main/README.md#github-external-collaborators\\n\\nPlease follow the procedure described there to grant @somegithubuser access to this repository.\\n\\nIf you have any questions, please post in #ask-operations-engineering on Slack.\\n"}]
+    }
+
+    it "calls github api" do
+      url = "https://api.github.com/repos/ministryofjustice/somerepo/issues"
+      expect(HttpClient).to receive(:new).and_return(http_client)
+      expect(http_client).to receive(:post_json).with(url, json)
+
+      ic.create
+    end
+  end
+end
