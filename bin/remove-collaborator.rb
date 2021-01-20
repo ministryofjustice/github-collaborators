0f805af19e7c625470e39e521fdf9be6c0990e8f
David Salgado <david@digitalronin.com>
2021-01-20 16:15:59 +0900

    Add a manual action removing collab via ruby code
    For some reason, this isn't working when it runs
    after the post JSON action, even though it looks
    like it's trying to do what it should.

---
+++ bin/remove-collaborator.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+# Fetch the "insufficiently defined collaborators" report as JSON, and remove
+# any collaborators who are not defined in terraform.
+
+require_relative "../lib/github_collaborators"
+
+def remove_collaborator(hash)
+  repository = hash.fetch("repository")
+  github_user = hash.fetch("login")
+
+  puts "Removing collaborator #{github_user} from repository #{repository}"
+
+  params = {
+    owner: ENV.fetch("OWNER"),
+    repository: repository,
+    github_user: github_user
+  }
+
+  # We must create the issue before removing access, because the issue is
+  # assigned to the removed collaborator, so that they (hopefully) get a
+  # notification about it.
+  puts "create issue:"
+  pp GithubCollaborators::IssueCreator.new(params).create
+  puts "remove collaborator:"
+  pp GithubCollaborators::AccessRemover.new(params).remove
+end
+
+############################################################
+
+c = {
+  "repository": "#{ENV.fetch("REPO")}",
+  "login": "#{ENV.fetch("USERNAME")}",
+  "status": "fail",
+  "issues": [
+    "Collaborator not defined in terraform"
+  ],
+  "href": "https://github.com/ministryofjustice/github-collaborators/blob/main/terraform/",
+  "defined_in_terraform": false,
+  "repo_url": "https://github.com/ministryofjustice/tax-tribunals-datacapture",
+  "login_url": "https://github.com/jriga",
+  "permission": "push",
+  "last_commit": "2021-01-18T13:00:46Z"
+}
+
+remove_collaborator(c)
