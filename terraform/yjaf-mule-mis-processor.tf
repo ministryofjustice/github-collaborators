f587267363984291a02ddb1a64639c507dd76c7c
David Salgado <david@digitalronin.com>
2021-01-28 18:10:59 +0900

    Remove duplicate entries for JWGNPS

--- terraform/yjaf-mule-mis-processor.tf
+++ terraform/yjaf-mule-mis-processor.tf
@@ -52,15 +52,5 @@ module "yjaf-mule-mis-processor" {
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
       review_after = "2021-12-11"
     },
-    {
-      github_user  = "JWGNPS"
-      permission   = "push"
-      name         = "James Grunewald"
-      email        = "james.grunewald@northgateps.com"
-      org          = "NPS (northgate)"
-      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
-      added_by     = "Greg Whiting <greg.whiting@northgateps.com> Devops for northgate"
-      review_after = "2022-01-27"
-    },
   ]
 }
