f39f6715e3f7daa41b231cba4b06d1c8b3624b92
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2021-02-04 13:42:59 +0000

    Add users to yjaf* repos

--- terraform/yjaf-case.tf
+++ terraform/yjaf-case.tf
@@ -32,5 +32,25 @@ module "yjaf-case" {
       added_by     = "Greg Whiting <greg.whiting@northgateps.com> Devops for northgate"
       review_after = "2022-01-27"
     },
+    {
+      github_user  = "brbaje-dev"
+      permission   = "admin"
+      name         = "Ben Bajek"
+      email        = "ben.bajek@northgateps.com"
+      org          = "NPS (northgate)"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "Greg Whiting <greg.whiting@northgateps.com> Devops for northgate"
+      review_after = "2021-12-01"
+    },
+    {
+      github_user  = "henrycarteruk"
+      permission   = "admin"
+      name         = "Henry Carter"
+      email        = "henry.carter@northgateps.com"
+      org          = "NPS (northgate)"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "Greg Whiting <greg.whiting@northgateps.com> Devops for northgate"
+      review_after = "2021-12-01"
+    },
   ]
 }
