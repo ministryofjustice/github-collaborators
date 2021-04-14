ea945f1fb89b1e247d54c9c97ddea915dd029089
Adrian Warman <adrian.warman@digital.justice.gov.uk>
2021-04-14 14:15:25 +0100

    Add Luke Crosby to list of security guidance collaborators.

--- terraform/security-guidance.tf
+++ terraform/security-guidance.tf
@@ -12,5 +12,15 @@ module "security-guidance" {
       added_by     = "adrian.warman@digital.justice.gov.uk"
       review_after = "2022-01-31"
     },
+    {
+      github_user  = "L-Crosby"
+      permission   = "pull"
+      name         = "Luke Crosby"
+      email        = "luke.crosby@digital.justice.gov.uk"
+      org          = "UK MoJ Digital and Technology"
+      reason       = "Reviewing and approving security policy and guidance content"
+      added_by     = "adrian.warman@digital.justice.gov.uk"
+      review_after = "2022-01-31"
+    },
   ]
 }
