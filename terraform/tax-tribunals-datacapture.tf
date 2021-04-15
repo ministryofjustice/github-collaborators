a5562e7cb0c448495313478d4368e962f554e7fe
AhsanZX97 <ahsan97@hotmail.co.uk>
2021-04-15 15:30:36 +0100

    Update tax-tribunals-datacapture.tf

--- terraform/tax-tribunals-datacapture.tf
+++ terraform/tax-tribunals-datacapture.tf
@@ -12,5 +12,15 @@ module "tax-tribunals-datacapture" {
       added_by     = "Sunil.Parmar@HMCTS.NET" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2021-06-01"             #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
+    {
+      github_user  = "AhsanZX97"
+      permission   = "push"
+      name         = "Ahsan Zia"            #  The name of the person behind github_user
+      email        = "ahsan.zia@hmcts.net"  #  Their email address
+      org          = "Solirius Consulting"    #  The organisation/entity they belong to
+      reason       = "Project maintainer"     #  Why is this person being granted access?
+      added_by     = "Sunil.Parmar@HMCTS.NET" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-06-01"             #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
   ]
 }
