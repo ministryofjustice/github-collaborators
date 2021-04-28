8349be83a3e55c1d77a6a1ce4ecd915061a23ce8
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2021-04-28 10:28:13 +0100

    add staff-infrastructure-monitoring-deployments

---
+++ terraform/staff-infrastructure-monitoring-deployments.tf
@@ -0,0 +1,36 @@
+module "staff-infrastructure-monitoring-deployments" {
+  source     = "./modules/repository-collaborators"
+  repository = "staff-infrastructure-monitoring-deployments"
+  collaborators = [
+    {
+      github_user  = "thip"
+      permission   = "admin"
+      name         = "David Capper"
+      email        = "david.capper@madetech.com"
+      org          = "Made Tech Ltd"
+      reason       = "PTTP Tech Team"
+      added_by     = "richard.baguley@justice.gov.uk"
+      review_after = "2021-06-01"
+    },
+    {
+      github_user  = "Themitchell"
+      permission   = "admin"
+      name         = "Andy Mitchell"
+      email        = "andrew.mitchell@madetech.com"
+      org          = "Made Tech Ltd"
+      reason       = "PTTP Tech Team"
+      added_by     = "richard.baguley@justice.gov.uk"
+      review_after = "2021-06-01"
+    },
+    {
+      github_user  = "elcorbs"
+      permission   = "admin"
+      name         = "Emma Corbett"
+      email        = "emma@madetech.com"
+      org          = "Made Tech Ltd"
+      reason       = "PTTP Tech Team"
+      added_by     = "richard.baguley@justice.gov.uk"
+      review_after = "2021-06-01"
+    },
+  ]
+}
