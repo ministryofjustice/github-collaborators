68a8fa4c3dbd3dca6356b29d8fea78014680fc2f
Ben Pirt <ben@pirt.co.uk>
2021-05-06 16:07:22 +0100

    Add User to the Bichard repos

--- terraform/bichard7-next.tf
+++ terraform/bichard7-next.tf
@@ -83,6 +83,16 @@ module "bichard7-next" {
       review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
+      github_user  = "daviesjamie"
+      permission   = "push"
+      name         = "Jamie Davies"                             #  The name of the person behind github_user
+      email        = "jamie.davies@madetech.com"                #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
       github_user  = "umarqureshi"
       permission   = "pull"
       name         = "Umar Qureshi"                        #  The name of the person behind github_user
