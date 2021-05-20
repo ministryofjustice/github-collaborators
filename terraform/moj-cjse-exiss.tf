d95bc00ba62524550da83450a408e3303a11f92d
Ben Pirt <ben@pirt.co.uk>
2021-05-20 13:35:49 +0100

    Update ExISS repo access

--- terraform/moj-cjse-exiss.tf
+++ terraform/moj-cjse-exiss.tf
@@ -11,6 +11,36 @@ module "moj-cjse-exiss" {
       reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
       added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "midlabcrisis"
+      permission   = "push"
+      name         = "Adam Taylor"
+      email        = "adam.taylor@digital.justice.gov.uk"
+      org          = "MoJ"
+      reason       = "CJSE Development"
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"
+      review_after = "2021-12-31"
+    },
+    {
+      github_user  = "graysok"
+      permission   = "push"
+      name         = "Kevin Hoskins"
+      email        = "kevin.hoskins@q-solution.co.uk"
+      org          = "Q Solution"
+      reason       = "CJSE Development"
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"
+      review_after = "2021-12-31"
+    },
+    {
+      github_user  = "flaxington"
+      permission   = "push"
+      name         = "Lee Flaxington"
+      email        = "lee.flaxington@q-solution.co.uk"
+      org          = "Q Solution"
+      reason       = "CJSE Development"
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"
+      review_after = "2021-12-31"
     }
   ]
 }
