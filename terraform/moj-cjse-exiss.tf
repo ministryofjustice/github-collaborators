0597299af336f733f08bd48599031b2016efb1f5
Ben Pirt <ben@pirt.co.uk>
2021-05-20 12:07:15 +0100

    Add the moj-cjse-exiss repo

---
+++ terraform/moj-cjse-exiss.tf
@@ -0,0 +1,16 @@
+module "moj-cjse-exiss" {
+  source     = "./modules/repository-collaborators"
+  repository = "moj-cjse-exiss"
+  collaborators = [
+    {
+      github_user  = "bjpirt"
+      permission   = "admin"
+      name         = "Ben Pirt"                                 #  The name of the person behind github_user
+      email        = "ben@madetech.com"                         #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    }
+  ]
+}
