ae9e6edd18c6e8562793f3df963376e351b2d9cb
Adrian Warman <30624085+warmanaMOJ@users.noreply.github.com>
2021-06-21 16:46:53 +0100

    Update security-guidance.tf
    Request addition of Greg Jotham to Security Guidance team.

--- terraform/security-guidance.tf
+++ terraform/security-guidance.tf
@@ -22,5 +22,15 @@ module "security-guidance" {
       added_by     = "adrian.warman@digital.justice.gov.uk"
       review_after = "2022-01-31"
     },
+      github_user  = "jothamgMOJ"
+      permission   = "push"
+      name         = "Greg Jotham"
+      email        = "greg.jotham@digital.justice.gov.uk"
+      org          = "UK MoJ Digital and Technology"
+      reason       = "Writing security policy and guidance content"
+      added_by     = "adrian.warman@digital.justice.gov.uk"
+      review_after = "2022-01-31"
+    },
+    {
   ]
 }
