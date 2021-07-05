d5c2a6b80930a45c6f10b3ffdd335a7ea34c7cd4
Gareth Davies <gareth.davies@digital.justice.gov.uk>
2021-07-05 11:41:38 +0100

    Remove BSI external collaborator from pin phone repos

--- terraform/hmpps-pin-phone-monitor.tf
+++ terraform/hmpps-pin-phone-monitor.tf
@@ -1,16 +1,6 @@
 module "hmpps-pin-phone-monitor" {
-  source     = "./modules/repository-collaborators"
+  source = "./modules/repository-collaborators"
   repository = "hmpps-pin-phone-monitor"
   collaborators = [
-    {
-      github_user  = "dwin998"
-      permission   = "pull"
-      name         = "Duncan Winfrey"
-      email        = "duncan.winfrey@bsicsiruk.cjsm.net"
-      org          = "BSI"
-      reason       = "Allow BSI pen testers to review internal code repo"
-      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of BSI"
-      review_after = "2021-04-10"
-    },
   ]
 }
