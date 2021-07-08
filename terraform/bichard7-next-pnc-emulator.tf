70f986bb8515aa11879df0e3369340f2f2c3bf4a
Ben Pirt <ben@pirt.co.uk>
2021-07-08 13:41:33 +0100

    Add new team member to Bichard repos

--- terraform/bichard7-next-pnc-emulator.tf
+++ terraform/bichard7-next-pnc-emulator.tf
@@ -91,6 +91,16 @@ module "bichard7-next-pnc-emulator" {
       reason       = "Security Scanning"                  #  Why is this person being granted access?
       added_by     = "Ben Pirt <ben@madetech.com>"        #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2021-12-31"                         #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "djr-made-tech"
+      permission   = "push"
+      name         = "David Roberts"                        #  The name of the person behind github_user
+      email        = "david.roberts@madetech.com" #  Their email address
+      org          = "Madetech"                                #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                  #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"        #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"                         #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     }
   ]
 }
