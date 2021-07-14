6f15b39aaa532e56164fcb5c25220b585e880714
Jim Gregory <49277801+JimGregory-SolTec@users.noreply.github.com>
2021-07-14 12:46:48 +0100

    Add PTTP team members

--- terraform/staff-device-management-windows10-apps.tf
+++ terraform/staff-device-management-windows10-apps.tf
@@ -52,5 +52,25 @@ module "staff-device-management-windows10-apps" {
       added_by     = "jgregory@sol-tec.com"  #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2021-12-31"            #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
+    {
+      github_user  = "calkin"
+      permission   = "push"
+      name         = "Josh Calkin"           #  The name of the person behind github_user
+      email        = "jcalkin@sol-tec.com"   #  Their email address
+      org          = "Sol-Tec"               #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"        #  Why is this person being granted access?
+      added_by     = "jgregory@sol-tec.com"  #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"            #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "mahmoudziadacc"
+      permission   = "push"
+      name         = "Mahmoud Ziada"         #  The name of the person behind github_user
+      email        = "mahmoud.ziada@contentandcloud.com" #  Their email address
+      org          = "Content+Cloud"         #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"        #  Why is this person being granted access?
+      added_by     = "jgregory@sol-tec.com"  #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"            #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    }
   ]
 }
