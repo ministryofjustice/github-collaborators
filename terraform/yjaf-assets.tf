296e2ec5eccf0291e310dd791d8e6af0c7e670ec
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2021-08-26 11:13:42 +0100

    Add collaborator

--- terraform/yjaf-assets.tf
+++ terraform/yjaf-assets.tf
@@ -142,5 +142,15 @@ module "yjaf-assets" {
       added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
       review_after = "2021-12-31"
     },
+    {
+      github_user  = "djv72"
+      permission   = "push"
+      name         = "David Vincent"
+      email        = "david.vincent@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
+      added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
+      review_after = "2021-12-31"
+    },
   ]
 }
