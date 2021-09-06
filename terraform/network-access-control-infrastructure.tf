3c91a542ee8a7242a80a95f93430e1bd81b4604a
Emile <emile.swarts@digital.justice.gov.uk>
2021-09-06 16:38:49 +0100

    Add myself to all the Network Access Control Service repos
    Lost access somehow, adding myself here.

--- terraform/network-access-control-infrastructure.tf
+++ terraform/network-access-control-infrastructure.tf
@@ -3,6 +3,16 @@ module "network-access-control-infrastructure" {
   repository = "network-access-control-infrastructure"
   collaborators = [
     {
+      github_user  = "emileswarts"
+      permission   = "admin"
+      name         = "Emile Swarts"
+      email        = "emile@madetech.com"
+      org          = "Made Tech Ltd"
+      reason       = "MoJ Network Access Control Tech Team"
+      added_by     = "justin.fielding@justice.gov.uk"
+      review_after = "2022-01-01"
+    },
+    {
       github_user  = "yusufsheiqh"
       permission   = "admin"
       name         = "Yusuf Sheikh"
