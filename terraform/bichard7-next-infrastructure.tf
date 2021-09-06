a393525e2f42e7e5f2d0198063ad5918f8bff8bb
Ben Pirt <ben@pirt.co.uk>
2021-09-06 14:32:11 +0100

    Reinstate Bichard users

--- terraform/bichard7-next-infrastructure.tf
+++ terraform/bichard7-next-infrastructure.tf
@@ -3,6 +3,26 @@ module "bichard7-next-infrastructure" {
   repository = "bichard7-next-infrastructure"
   collaborators = [
     {
+      github_user  = "bjpirt"
+      permission   = "admin"
+      name         = "Ben Pirt"                                 #  The name of the person behind github_user
+      email        = "ben@madetech.com"                         #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "brettminnie"
+      permission   = "admin"
+      name         = "Brett Minnie"                             #  The name of the person behind github_user
+      email        = "brett.minnie@madetech.com"                #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
       github_user  = "sioldham"
       permission   = "push"
       name         = "Simon Oldham"                             #  The name of the person behind github_user
