769e8e41f8c48cdb90a46dfe6cd6295b1591690f
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2021-09-07 11:28:57 +0100

    More GH provider fixing

--- terraform/modules/repository-collaborators/versions.tf
+++ terraform/modules/repository-collaborators/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     github = {
-      version = "4.0"
       source  = "integrations/github"
+      version = "~> 4.0"
     }
   }
 }
