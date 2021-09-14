43f983b8160569aece618da2eb83313d2942a1a9
Ben Ashton <ben@automationlogic.com>
2021-09-14 14:22:00 +0100

    Create issues for upcoming review date expiration

--- lib/github_collaborators/issue_creator.rb
+++ lib/github_collaborators/issue_creator.rb
@@ -13,7 +13,22 @@ class GithubCollaborators
       HttpClient.new.post_json(url, issue_hash.to_json)
     end
 
+    def create_review_date
+      if get_issues_for_user.empty?
+        url = "https://api.github.com/repos/#{owner}/#{repository}/issues"
+        HttpClient.new.post_json(url, issue_hash_review_after.to_json)
+      end
+    end
+
+    def get_issues_for_user
+      url = "https://api.github.com/repos/#{owner}/#{repository}/issues"
+      response = HttpClient.new.fetch_json(url).body
+      JSON.parse(response).select { |x| x["assignee"]["login"] == github_user}
+    end
+
     private
+
+
 
     def issue_hash
       {
@@ -26,6 +41,23 @@ class GithubCollaborators
           https://github.com/ministryofjustice/github-collaborators/blob/main/README.md#github-external-collaborators
           
           Please follow the procedure described there to grant @#{github_user} access to this repository.
+          
+          If you have any questions, please post in #ask-operations-engineering on Slack.
+        EOF
+      }
+    end
+
+    def issue_hash_review_after
+      {
+        title: "Review after date expiry is upcoming for user: #{github_user}",
+        assignees: [github_user],
+        body: <<~EOF
+          Hi there
+          
+          The user @#{github_user} has its access for this repository maintained in code here:
+          https://github.com/ministryofjustice/github-collaborators/blob/main/README.md#github-external-collaborators
+
+          The review_after date is due to expire within one month, please update this via a PR if they still require access.
           
           If you have any questions, please post in #ask-operations-engineering on Slack.
         EOF
