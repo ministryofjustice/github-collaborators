97e6f85a49e8bf7b701e60a655bbe220e3690740
ben-al <61050986+ben-al@users.noreply.github.com>
2021-09-17 09:49:36 +0100

    Create issues for upcoming review date expiration (#262)
    * Create issues for upcoming review date expiration

--- bin/external-collaborators.rb
+++ bin/external-collaborators.rb
@@ -2,14 +2,34 @@
 
 require_relative "../lib/github_collaborators"
 
+login = "ministryofjustice"
+base_url = "https://github.com/ministryofjustice/github-collaborators/blob/main/terraform"
+
+# Retrieve list of all failed external collaborators
 list = GithubCollaborators::OrganizationExternalCollaborators.new(
-  login: "ministryofjustice",
-  base_url: "https://github.com/ministryofjustice/github-collaborators/blob/main/terraform"
+  login: login,
+  base_url: base_url
 ).list
 
+# Prepare payload for report
 output = {
   data: list,
   updated_at: Time.now.strftime("%Y-%m-%d %H:%M:%S")
 }
 
+# Loop through failed external collaborators
+list.each do |x|
+  # If issues include review date being within a month, create an issue on the repo
+  if x["issues"].include? "Review after date is within a month"
+    params = {
+      owner: login,
+      repository: x["repository"],
+      github_user: x["login"]
+    }
+    # Create issue
+    GithubCollaborators::IssueCreator.new(params).create_review_date
+  end
+end
+
+# Output for report
 puts output.to_json
--- spec/issue_creator_spec.rb
+++ spec/issue_creator_spec.rb
@@ -23,5 +23,15 @@ class GithubCollaborators
 
       ic.create
     end
+
+    it "returns present issues" do
+      url = "https://api.github.com/repos/ministryofjustice/somerepo/issues"
+      response = Net::HTTPSuccess.new(1.0, "200", "OK")
+      expect(response).to receive(:body) { '[{"assignee":{"login": "somegithubuser"}}]' }
+      expect(HttpClient).to receive(:new).and_return(http_client)
+      expect(http_client).to receive(:fetch_json).and_return(response)
+
+      expect(ic.get_issues_for_user).not_to be_empty
+    end
   end
 end
