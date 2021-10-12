749df79c6a0acae4cda216c9ef97e0a3d8ad595c
ben-al <61050986+ben-al@users.noreply.github.com>
2021-10-12 14:12:49 +0100

    Add functionality to detect poor file extensions (#289)
    Add functionality to detect poor file extensions

---
+++ scripts/validate-commit-files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+check_file_extensions(){
+
+  # Set RC switch as fine to begin
+  RC_FLAG=0
+  # Fetch main
+  git fetch origin main:refs/remotes/origin/main
+
+  # Look for files missing correct extension
+  for file in $(git diff origin/main --name-only -- terraform/); do
+    if [[ ${file} != *.tf ]]; then
+      RC_FLAG=1
+      echo "${file} does not have the correct .tf extension"
+    fi
+  done
+
+
+  if [[ ${RC_FLAG} == 1 ]]; then
+    exit 1
+  fi
+}
+
+check_file_extensions
