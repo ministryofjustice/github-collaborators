b6491fd1f36e15f445b0d972c0fedc09bd8960ae
Ben Ashton <ben@automationlogic.com>
2021-10-19 13:03:28 +0100

    Add Ben Bajek and Thomas Tipler to all yjaf and yjsm repos

--- terraform/YJAF-POC.tf
+++ terraform/YJAF-POC.tf
@@ -12,5 +12,25 @@ module "YJAF-POC" {
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
       review_after = "2021-12-11"
     },
+    {
+      github_user  = "brbaje-dev"
+      permission   = "admin"
+      name         = "Ben Bajek"
+      email        = "ben.bajek@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "Devops guys need access to make app/infra changes"
+      added_by     = "Greg Whiting - greg.whiting@northgateps.com"
+      review_after = "2022-12-31"
+    },
+    {
+      github_user  = "ttipler"
+      permission   = "admin"
+      name         = "Thomas Tipler"
+      email        = "thomas.tipler@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "Devops guys need access to make app/infra changes"
+      added_by     = "Greg Whiting - greg.whiting@northgateps.com"
+      review_after = "2022-12-31"
+    },
   ]
 }
