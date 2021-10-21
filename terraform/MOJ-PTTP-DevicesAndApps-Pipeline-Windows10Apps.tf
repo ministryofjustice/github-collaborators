ecb58d8fc9b97de30c08bb739e329cbead3e7b52
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2021-10-21 11:13:19 +0100

    Replace collaborator github_user

--- terraform/MOJ-PTTP-DevicesAndApps-Pipeline-Windows10Apps.tf
+++ terraform/MOJ-PTTP-DevicesAndApps-Pipeline-Windows10Apps.tf
@@ -3,7 +3,7 @@ module "MOJ-PTTP-DevicesAndApps-Pipeline-Windows10Apps" {
   repository = "MOJ.PTTP.DevicesAndApps.Pipeline.Windows10Apps"
   collaborators = [
     {
-      github_user  = "JimGregory-SolTec"
+      github_user  = "JimGregory-CplusC"
       permission   = "admin"
       name         = "Jim Gregory"                   #  The name of the person behind github_user
       email        = "jgregory@sol-tec.com"          #  Their email address
