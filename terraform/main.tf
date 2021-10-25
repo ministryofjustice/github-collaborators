47bfaf966c114f8072249176cad9ccc0ca770383
“liban-AL” <liban@automationlogic.com>
2021-10-25 16:07:35 +0100

    ^

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  # `backend` blocks do not support variables, so the following are hard-coded here:
-  # - S3 bucket name, which is created [here](https://github.com/ministryofjustice/cloud-platform-environments/blob/main/namespaces/live-1.cloud-platform.service.justice.gov.uk/operations-engineering/resources/s3.tf)
-  backend "s3" {
-    encrypt = true
-    key     = "github-collaborators/terraform.tfstate"
-  }
-}
-
 provider "github" {
   owner = "ministryofjustice"
   token = var.github_token
