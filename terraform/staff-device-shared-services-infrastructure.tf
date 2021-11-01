9a925db9e22e5dc1109d07d32ea2d8ac145423d7
Richard Baguley <20583399+bagg3rs@users.noreply.github.com>
2021-11-01 14:57:08 +0000

    :fire: remove MadeTech contractors RichR and NeilKidd who is added via Org now

--- terraform/staff-device-shared-services-infrastructure.tf
+++ terraform/staff-device-shared-services-infrastructure.tf
@@ -31,26 +31,6 @@ module "staff-device-shared-services-infrastructure" {
       reason       = "MoJ Network Access Control Tech Team"
       added_by     = "justin.fielding@justice.gov.uk"
       review_after = "2022-01-01"
-    },
-    {
-      github_user  = "richrace"
-      permission   = "admin"
-      name         = "Richard Race"
-      email        = "richard.race@madetech.com"
-      org          = "Made Tech Ltd"
-      reason       = "PTTP Tech Team"
-      added_by     = "richard.baguley@digital.justice.gov.uk"
-      review_after = "2021-10-31"
-    },
-    {
-      github_user  = "neilkidd"
-      permission   = "admin"
-      name         = "Neil Kidd"
-      email        = "neil.kidd@madetech.com"
-      org          = "Made Tech Ltd"
-      reason       = "PTTP Tech Team"
-      added_by     = "aaron.robinson@digital.justice.gov.uk"
-      review_after = "2022-04-31"
     }
   ]
 }
