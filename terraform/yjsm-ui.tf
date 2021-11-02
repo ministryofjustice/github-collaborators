bc04dc372071aac2362f3da7e6452da237c82058
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2021-11-02 09:43:48 +0000

    Remove collaborator

--- terraform/yjsm-ui.tf
+++ terraform/yjsm-ui.tf
@@ -53,16 +53,6 @@ module "yjsm-ui" {
       review_after = "2021-12-31"
     },
     {
-      github_user  = "boydingham"
-      permission   = "push"
-      name         = "Boyd Cunningham"
-      email        = "Boyd.cunningham@northgateps.com"
-      org          = "NPS (northgate)"
-      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
-      added_by     = "Greg Whiting <greg.whiting@northgateps.com> Devops for northgate"
-      review_after = "2021-12-31"
-    },
-    {
       github_user  = "brbaje-dev"
       permission   = "admin"
       name         = "Ben Bajek"
