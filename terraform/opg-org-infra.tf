509197e93aabe28abdfbc7d7e1125e8173c814f8
Tom Withers <thomas.withers@digital.justice.gov.uk>
2021-12-03 16:10:43 +0000

    Remove BSI user as a collaborator, ITHC Finished

--- terraform/opg-org-infra.tf
+++ terraform/opg-org-infra.tf
@@ -2,15 +2,5 @@ module "opg-org-infra" {
   source     = "./modules/repository-collaborators"
   repository = "opg-org-infra"
   collaborators = [
-    {
-      github_user  = "bsi-thudson"
-      permission   = "pull"
-      name         = "Thomas Hudson"
-      email        = "thomas.hudson@bsigroup.com"
-      org          = "BSI"
-      reason       = "ITHC Testing"
-      added_by     = "thomas.withers@justice.gov.uk"
-      review_after = "2021-12-06"
-    }
   ]
 }
