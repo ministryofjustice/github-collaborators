2cdcf5d27c9d88c042d13c11a188e39159b7455a
ben-al <61050986+ben-al@users.noreply.github.com>
2021-12-09 11:32:36 +0000

    Add new collaborators (#380)
    * Add new collaborators
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- terraform/staff-device-management-virtualdesktop.tf
+++ terraform/staff-device-management-virtualdesktop.tf
@@ -5,42 +5,42 @@ module "staff-device-management-virtualdesktop" {
     {
       github_user  = "calkin"
       permission   = "push"
-      name         = "Josh Calkin"                   #  The name of the person behind github_user
-      email        = "jcalkin@sol-tec.com"           #  Their email address
-      org          = "Sol-Tec"                       #  The organisation/entity they belong to
-      reason       = "PTTP Tech team"                #  Why is this person being granted access?
-      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                    #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Josh Calkin"                     #  The name of the person behind github_user
+      email        = "josh.calkin@contentandcloud.com" #  Their email address
+      org          = "Content and Cloud"               #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"                  #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk"   #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "RyanLWilliams"
       permission   = "push"
-      name         = "Ryan Williams"                 #  The name of the person behind github_user
-      email        = "rwilliams@sol-tec.com"         #  Their email address
-      org          = "Sol-Tec"                       #  The organisation/entity they belong to
-      reason       = "PTTP Tech team"                #  Why is this person being granted access?
-      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                    #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Ryan Williams"                     #  The name of the person behind github_user
+      email        = "ryan.williams@contentandcloud.com" #  Their email address
+      org          = "Content and Cloud"                 #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"                    #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk"     #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "VinceThompson"
       permission   = "push"
       name         = "Vince Thompson"                #  The name of the person behind github_user
-      email        = "vthompson@sol-tec.com"         #  Their email address
-      org          = "Sol-Tec"                       #  The organisation/entity they belong to
+      email        = "vthompson@contentandcloud.com" #  Their email address
+      org          = "Content and Cloud"             #  The organisation/entity they belong to
       reason       = "PTTP Tech Team"                #  Why is this person being granted access?
       added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                    #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2022-12-31"                    #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "jabenny"
       permission   = "push"
       name         = "Josh Benfield"                                                     #  The name of the person behind github_user
-      email        = "jbenfield@sol-tec.com"                                             #  Their email address
-      org          = "Sol-Tec"                                                           #  The organisation/entity they belong to
+      email        = "josh.benfield@ontentandcloud.com"                                  #  Their email address
+      org          = "Content and Cloud"                                                 #  The organisation/entity they belong to
       reason       = "PTTP Tech Team"                                                    #  Why is this person being granted access?
       added_by     = "MoJ Technical Operations <MoJ-TechnicalOperations@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2022-12-31"                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
 }
--- terraform/staff-infrastructure-azure-landing-zone-spoke-eucs.tf
+++ terraform/staff-infrastructure-azure-landing-zone-spoke-eucs.tf
@@ -5,42 +5,42 @@ module "staff-infrastructure-azure-landing-zone-spoke-eucs" {
     {
       github_user  = "HughSolTec"
       permission   = "admin"
-      name         = "Hugh Dingwall"                 #  The name of the person behind github_user
-      email        = "hdingwall@sol-tec.com"         #  Their email address
-      org          = "Sol-Tec"                       #  The organisation/entity they belong to
-      reason       = "PTTP Tech Team"                #  Why is this person being granted access?
-      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                    #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Hugh Dingwall"                     #  The name of the person behind github_user
+      email        = "hugh.dingwall@contentandcloud.com" #  Their email address
+      org          = "content and cloud"                 #  The organisation/entity they belong to
+      reason       = "PTTP Tech Team"                    #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk"     #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "RyanLWilliams"
       permission   = "push"
-      name         = "Ryan Williams"                 #  The name of the person behind github_user
-      email        = "rwilliams@sol-tec.com"         #  Their email address
-      org          = "Sol-Tec"                       #  The organisation/entity they belong to
-      reason       = "PTTP Tech team"                #  Why is this person being granted access?
-      added_by     = "matthew.white1@justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                    #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Ryan Williams"                     #  The name of the person behind github_user
+      email        = "ryan.williams@contentandcloud.com" #  Their email address
+      org          = "content and cloud"                 #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"                    #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk"     #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "LiliNedeleva"
       permission   = "push"
-      name         = "Lili Nedeleva"         #  The name of the person behind github_user
-      email        = "lnedeleva@sol-tec.com" #  Their email address
-      org          = "Sol-Tec"               #  The organisation/entity they belong to
-      reason       = "PTTP Tech team"        #  Why is this person being granted access?
-      added_by     = "jgregory@sol-tec.com"  #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"            #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Lili Nedeleva"                     #  The name of the person behind github_user
+      email        = "lili.nedeleva@contentandcloud.com" #  Their email address
+      org          = "content and cloud"                 #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"                    #  Why is this person being granted access?
+      added_by     = "jim.gregory@contentandcloud.com"   #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
       github_user  = "calkin"
       permission   = "push"
-      name         = "Josh Calkin"          #  The name of the person behind github_user
-      email        = "jcalkin@sol-tec.com"  #  Their email address
-      org          = "Sol-Tec"              #  The organisation/entity they belong to
-      reason       = "PTTP Tech team"       #  Why is this person being granted access?
-      added_by     = "jgregory@sol-tec.com" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"           #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      name         = "Josh Calkin"                     #  The name of the person behind github_user
+      email        = "josh.calkin@contentandcloud.com" #  Their email address
+      org          = "Content and Cloud"               #  The organisation/entity they belong to
+      reason       = "PTTP Tech team"                  #  Why is this person being granted access?
+      added_by     = "jim.gregory@contentandcloud.com" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     }
   ]
 }
