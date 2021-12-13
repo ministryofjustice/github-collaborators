0aeb44cdae4b7e9c40c6dbf43e3e7f14cee8c774
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2021-12-13 15:51:40 +0000

    Update collaborator review_after dates

--- terraform/yjaf-assets.tf
+++ terraform/yjaf-assets.tf
@@ -10,7 +10,7 @@ module "yjaf-assets" {
       org          = "Northgate"
       reason       = "Part of the Northgate supplier team for the YJB YJAF system"
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
-      review_after = "2021-12-11"
+      review_after = "2022-12-13"
     },
     {
       github_user  = "gregi2n"
@@ -20,7 +20,7 @@ module "yjaf-assets" {
       org          = "Northgate"
       reason       = "Part of the Northgate supplier team for the YJB YJAF system"
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
-      review_after = "2021-12-11"
+      review_after = "2022-12-13"
     },
     {
       github_user  = "griffinjuknps"
@@ -40,7 +40,7 @@ module "yjaf-assets" {
       org          = "NEC Software Solutions"
       reason       = "Part of the Northgate supplier team for the YJB YJAF system"
       added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
-      review_after = "2021-12-11"
+      review_after = "2022-12-13"
     },
     {
       github_user  = "VasilDimitrov22"
