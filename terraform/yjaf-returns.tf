8b1f0b1ed45b520c0c78b21deade38437a330163
Nick Walters <nick.walters@digital.justice.gov.uk>
2021-12-17 16:09:59 +0000

    test

--- terraform/yjaf-returns.tf
+++ terraform/yjaf-returns.tf
@@ -9,7 +9,7 @@ module "yjaf-returns" {
       email        = "greg.whiting@northgateps.com"
       org          = "Northgate"
       reason       = "Part of the Northgate supplier team for the YJB YJAF system"
-      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
+      added_by     = "<gareth.davies@digita345l.justice.gov.uk> on behalf of the YJB"
       review_after = "2022-12-13"
     },
     {
