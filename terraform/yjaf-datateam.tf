beabde5ff7795e1fc61b0e63f01090bc7d4d0d00
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2021-12-20 16:27:24 +0000

    Update MoJ added_up

--- terraform/yjaf-datateam.tf
+++ terraform/yjaf-datateam.tf
@@ -9,7 +9,7 @@ module "yjaf-datateam" {
       email        = "sam.mohammed@necsws.com"
       org          = "NEC"
       reason       = "For YJAF data team to store their scripts into the new yjaf data team repo"
-      added_by     = "joanna.harvey@necsws.com"
+      added_by     = "antony.bishop@digital.justice.gov.uk"
       review_after = "2022-12-19"
     },
     {
@@ -19,7 +19,7 @@ module "yjaf-datateam" {
       email        = "ann.meads-tallis@necsws.com"
       org          = "NEC"
       reason       = "For YJAF data team to store their scripts into the new yjaf data team repo"
-      added_by     = "joanna.harvey@necsws.com"
+      added_by     = "antony.bishop@digital.justice.gov.uk"
       review_after = "2022-12-19"
     },
   ]
