127a7164db170288ca316c8e40999009c1a0090b
Nick Walters <nick.walters@digital.justice.gov.uk>
2021-12-20 13:09:42 +0000

    test12

--- terraform/yjaf-returns.tf
+++ terraform/yjaf-returns.tf
@@ -39,7 +39,7 @@ module "yjaf-returns" {
       email        = "muhammad.anjum@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = " bob@jus6tice.gov.uk " #  Who made the decision to grant them access? e.g. Awesome Team <awesome.team@digital.justice.gov.uk> 
+      added_by     = " bob@justice.gov.uk " #  Who made the decision to grant them access? e.g. Awesome Team <awesome.team@digital.justice.gov.uk> 
       review_after = "2021-12-31"
     },
     {
@@ -49,7 +49,7 @@ module "yjaf-returns" {
       email        = "muhammad.anjum@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = " bob@digital.justice.gov.uk" #  Who made the decision to grant them access? e.g. Awesome Team <awesome.team@digital.justice.gov.uk> 
+      added_by     = " bob@digital.just6ice.gov.uk" #  Who made the decision to grant them access? e.g. Awesome Team <awesome.team@digital.justice.gov.uk> 
       review_after = "2021-12-31"
     },
     {
