a74f77e2e2cf6ec4e6e91c9ca02f05e789a53070
Nick Walters <nick.walters@digital.justice.gov.uk>
2021-12-20 10:43:23 +0000

    test1

--- terraform/yjaf-returns.tf
+++ terraform/yjaf-returns.tf
@@ -43,6 +43,16 @@ module "yjaf-returns" {
       review_after = "2021-12-31"
     },
     {
+      github_user  = "waheedanjum"
+      permission   = "push"
+      name         = "Muhammad Waheed Anjum"
+      email        = "muhammad.anjum@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
+      added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
+      review_after = "2021-12-31"
+    },
+    {
       github_user  = "griffinjuknps"
       permission   = "admin"
       name         = "Jeremy Griffin"
