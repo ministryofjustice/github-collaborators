aaef60d5911e3e99030b4783cc753166464e970f
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-01-17 16:34:44 +0000

    Remove collaborators
    Code owner confirmed collaborators could be removed.

--- terraform/moj-cjse-exiss.tf
+++ terraform/moj-cjse-exiss.tf
@@ -10,37 +10,7 @@ module "moj-cjse-exiss" {
       org          = "Madetech"                                 #  The organisation/entity they belong to
       reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
       added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2021-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2022-06-17"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
-    {
-      github_user  = "midlabcrisis"
-      permission   = "push"
-      name         = "Adam Taylor"
-      email        = "adam.taylor@digital.justice.gov.uk"
-      org          = "MoJ"
-      reason       = "CJSE Development"
-      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"
-      review_after = "2021-12-31"
-    },
-    {
-      github_user  = "graysok"
-      permission   = "push"
-      name         = "Kevin Hoskins"
-      email        = "kevin.hoskins@q-solution.co.uk"
-      org          = "Q Solution"
-      reason       = "CJSE Development"
-      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"
-      review_after = "2021-12-31"
-    },
-    {
-      github_user  = "flaxington"
-      permission   = "push"
-      name         = "Lee Flaxington"
-      email        = "lee.flaxington@q-solution.co.uk"
-      org          = "Q Solution"
-      reason       = "CJSE Development"
-      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>"
-      review_after = "2021-12-31"
-    }
   ]
 }
