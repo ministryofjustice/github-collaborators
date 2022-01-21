544ae2d8bcb73ab3d0218ca6c5439104d1733134
MatthewWhiteMoJ <61458526+MatthewWhiteMoJ@users.noreply.github.com>
2022-01-21 09:45:12 +0000

    Update staff-device-management-virtualdesktop.tf

--- terraform/staff-device-management-virtualdesktop.tf
+++ terraform/staff-device-management-virtualdesktop.tf
@@ -42,5 +42,15 @@ module "staff-device-management-virtualdesktop" {
       added_by     = "MoJ Technical Operations <MoJ-TechnicalOperations@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-12-31"                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
+    {
+      github_user  = "raghavkuchipudi"
+      permission   = "pull"
+      name         = "Raghav Kuchipudi"
+      email        = "raghav.kuchipudi@contentandcloud.com"
+      org          = "Content and Cloud"
+      reason       = "New team member / PTTP tech team"
+      added_by     = "matthew.white1@justice.gov.uk"
+      review_after = "2022-12-31"
+    }
   ]
 }
