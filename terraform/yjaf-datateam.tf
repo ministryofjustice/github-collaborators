4a9869e4256b62682b593bf00b1a0ba2c327d9b4
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-02-14 11:21:13 +0000

    Add new collaborators

--- terraform/yjaf-datateam.tf
+++ terraform/yjaf-datateam.tf
@@ -32,5 +32,25 @@ module "yjaf-datateam" {
       added_by     = "Jon Dent jon.dent@justice.gov.uk"
       review_after = "2022-12-31"
     },
+    {
+      github_user  = "InFlamesForever"
+      permission   = "admin"
+      name         = "Richard Came"
+      email        = "richard.came@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
+      added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
+      review_after = "2022-08-27"
+    },
+    {
+      github_user  = "richardbradwell"
+      permission   = "admin"
+      name         = "Richard Bradwell"
+      email        = "richard.bradwell@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
+      added_by     = "Jo Harvey <joanna.harvey@necsws.com"
+      review_after = "2022-10-05"
+    },
   ]
 }
