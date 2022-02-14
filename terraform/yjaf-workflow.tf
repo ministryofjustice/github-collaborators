e28adfe07f81ae900bb9c7f011e7d1d8fa723541
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-02-14 11:33:20 +0000

    Change collaborator permissions

--- terraform/yjaf-workflow.tf
+++ terraform/yjaf-workflow.tf
@@ -54,7 +54,7 @@ module "yjaf-workflow" {
     },
     {
       github_user  = "InFlamesForever"
-      permission   = "push"
+      permission   = "admin"
       name         = "Richard Came"
       email        = "richard.came@necsws.com"
       org          = "NEC Software Solutions"
@@ -64,7 +64,7 @@ module "yjaf-workflow" {
     },
     {
       github_user  = "richardbradwell"
-      permission   = "push"
+      permission   = "admin"
       name         = "Richard Bradwell"
       email        = "richard.bradwell@necsws.com"
       org          = "NEC Software Solutions"
