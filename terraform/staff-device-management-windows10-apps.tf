c553de5b6b740fae99f8736282a4e789b4634928
Lili Nedeleva <lili.nedeleva@contentandcloud.com>
2022-02-24 09:28:15 +0000

    Revoking jcalkin access, giving jim gregory admin access

--- terraform/staff-device-management-windows10-apps.tf
+++ terraform/staff-device-management-windows10-apps.tf
@@ -43,16 +43,6 @@ module "staff-device-management-windows10-apps" {
       review_after = "2022-12-31"                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
-      github_user  = "calkin"
-      permission   = "push"
-      name         = "Josh Calkin"                     #  The name of the person behind github_user
-      email        = "josh.calkin@contentandcloud.com" #  Their email address
-      org          = "Content and Cloud"               #  The organisation/entity they belong to
-      reason       = "PTTP Tech team"                  #  Why is this person being granted access?
-      added_by     = "matthew.white1@justice.gov.uk"   #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-12-31"                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
       github_user  = "cyrusirandoust"
       permission   = "push"
       name         = "Cyrus Irandoust"                     #  The name of the person behind github_user
