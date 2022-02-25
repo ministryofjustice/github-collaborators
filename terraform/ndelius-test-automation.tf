7d7cc354c34b306ca0e96c6c9d918275d03ec42d
Marcus Aspin <maspin@unilink.com>
2022-02-25 09:13:25 +0000

    Add Michal Laskowski to ndelius-test-automation

--- terraform/ndelius-test-automation.tf
+++ terraform/ndelius-test-automation.tf
@@ -1,4 +1,4 @@
-module "ndelius-test-automation" {
+module "ndelius-test-automation-swestb" {
   source     = "./modules/repository-collaborators"
   repository = "ndelius-test-automation"
   collaborators = [
@@ -11,6 +11,23 @@ module "ndelius-test-automation" {
       reason       = "Stuart is a new member of the team and will require the same level of access to the repos as the rest of the developers in this group" #  Why is this person being granted access?
       added_by     = "Probation Infrastructure AWS Team, awssupportteam@digital.justice.gov.uk"                                                              #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-05-21"                                                                                                                            #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+  ]
+}
+  
+module "ndelius-test-automation-mlaskowski4" {
+  source     = "./modules/repository-collaborators"
+  repository = "ndelius-test-automation"
+  collaborators = [
+    {
+      github_user  = "mlaskowski4"
+      permission   = "admin"
+      name         = "Michal Laskowski"                                                                  #  The name of the person behind github_user
+      email        = "mlaskowski@unilink.com"                                                            #  Their email address
+      org          = "Unilink"                                                                           #  The organisation/entity they belong to
+      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS" #  Why is this person being granted access?
+      added_by     = "Marcus Aspin <maspin@unilink.com>"                                                 #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2023-02-25"                                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
 }
