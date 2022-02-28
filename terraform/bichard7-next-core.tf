23cc0b05f9973f69c5261ee574a53c42d15220a2
Ben Pirt <ben@pirt.co.uk>
2022-02-28 12:01:24 +0000

    Add new users to Bichard repos

--- terraform/bichard7-next-core.tf
+++ terraform/bichard7-next-core.tf
@@ -71,6 +71,26 @@ module "bichard7-next-core" {
       reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
       added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "C-gyorfi"
+      permission   = "push"
+      name         = "Csaba Gyorfi"                             #  The name of the person behind github_user
+      email        = "csaba@madetech.com"                       #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "tomvaughan77"
+      permission   = "push"
+      name         = "Tom Vaughan"                              #  The name of the person behind github_user
+      email        = "jazz.sarkaria@madetech.com"               #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     }
   ]
 }
