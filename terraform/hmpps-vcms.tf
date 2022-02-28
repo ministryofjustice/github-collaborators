d702c1a5d7b3a4f5e8cc936d4498b4768d43ba86
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-02-28 15:35:36 +0000

    add-Miriam-Gomez-Orozco to hmpps-vcms.tf (#524)

--- terraform/hmpps-vcms.tf
+++ terraform/hmpps-vcms.tf
@@ -22,5 +22,15 @@ module "hmpps-vcms" {
       added_by     = "Probation Infrastructure AWS Team, awssupportteam@digital.justice.gov.uk"          #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-05-21"                                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
+    {
+      github_user  = "miriamgo-civica"
+      permission   = "maintain"
+      name         = "Miriam Gomez-Orozco"                                                          #  The name of the person behind github_user
+      email        = "Miriam.Gomez-Orozco@civica.co.uk"                                             #  Their email address
+      org          = "Civica"                                                                       #  The organisation/entity they belong to
+      reason       = "Civica developer for Victims Case Management System"                          #  Why is this person being granted access?
+      added_by     = "Probation Infrastructure AWS Team, maximillian.lakanu@digital.justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2023-02-27"                                                                   #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
   ]
 }
