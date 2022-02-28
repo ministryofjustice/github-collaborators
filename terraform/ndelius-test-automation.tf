57284045c807efcfffc3804061132af44f097004
Marcus Aspin <maspin@unilink.com>
2022-02-28 10:00:30 +0000

    Add approver for access request

--- terraform/ndelius-test-automation.tf
+++ terraform/ndelius-test-automation.tf
@@ -19,7 +19,7 @@ module "ndelius-test-automation" {
       email        = "mlaskowski@unilink.com"                                                            #  Their email address
       org          = "Unilink"                                                                           #  The organisation/entity they belong to
       reason       = "To enable Unilink to continue supplying development and testing services to HMPPS" #  Why is this person being granted access?
-      added_by     = "Marcus Aspin <maspin@unilink.com>"                                                 #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"                              #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2023-02-25"                                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
