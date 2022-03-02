a69731c7f17c74e99a1c01d2dd7aa6c77be9b3b1
marcus.aspin <maspin@unilink.com>
2022-03-02 15:40:46 +0000

    Grant access for Sean Valmonte to hmpps-delius-api
    Also reduces existing access from "admin" to "push"

--- terraform/hmpps-delius-api.tf
+++ terraform/hmpps-delius-api.tf
@@ -4,7 +4,7 @@ module "hmpps-delius-api" {
   collaborators = [
     {
       github_user  = "mlaskowski4"
-      permission   = "admin"
+      permission   = "push"
       name         = "Michal Laskowski"                                                                  #  The name of the person behind github_user
       email        = "mlaskowski@unilink.com"                                                            #  Their email address
       org          = "Unilink"                                                                           #  The organisation/entity they belong to
@@ -14,9 +14,19 @@ module "hmpps-delius-api" {
     },
     {
       github_user  = "peter-bcl"
-      permission   = "admin"
+      permission   = "push"
       name         = "Peter Wilson"                                                                      #  The name of the person behind github_user
       email        = "pwilson@unilink.com"                                                               #  Their email address
+      org          = "Unilink"                                                                           #  The organisation/entity they belong to
+      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS" #  Why is this person being granted access?
+      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"                              #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2023-02-25"                                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "Seanvalmonte"
+      permission   = "push"
+      name         = "Sean Valmonte"                                                                     #  The name of the person behind github_user
+      email        = "svalmonte@unilink.com"                                                             #  Their email address
       org          = "Unilink"                                                                           #  The organisation/entity they belong to
       reason       = "To enable Unilink to continue supplying development and testing services to HMPPS" #  Why is this person being granted access?
       added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"                              #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
