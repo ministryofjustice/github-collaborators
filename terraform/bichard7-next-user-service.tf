21d287b4966858fb265c93f870b33dff5517bc82
Ben Pirt <ben@pirt.co.uk>
2022-03-08 15:03:25 +0000

    Remove Mihai

--- terraform/bichard7-next-user-service.tf
+++ terraform/bichard7-next-user-service.tf
@@ -43,16 +43,6 @@ module "bichard7-next-user-service" {
       review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
-      github_user  = "MihaiMadeT"
-      permission   = "push"
-      name         = "Mihai-Alexandru Popa-Matei"               #  The name of the person behind github_user
-      email        = "mihai.popa-matai@madetech.com"            #  Their email address
-      org          = "Madetech"                                 #  The organisation/entity they belong to
-      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
-      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
       github_user  = "monotypical"
       permission   = "push"
       name         = "Alice Lee"                                #  The name of the person behind github_user
