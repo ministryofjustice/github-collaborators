6b9980e0bac5c32a35e2fbdba7f407e9a4a1c593
federicostaianojustice1 <86769850+federicostaianojustice1@users.noreply.github.com>
2022-03-24 10:59:51 +0000

    Update pfs-software-services_ansible.tf
    Reviewing collaborators for this Repo

--- terraform/pfs-software-services_ansible.tf
+++ terraform/pfs-software-services_ansible.tf
@@ -22,31 +22,12 @@ module "pfs-software-services_ansible" {
       added_by     = "Incell Infrastructure team, federico.staiano1@justice.gov.uk"                                    #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-06-18"                                                                                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
+   
     {
-      github_user  = "jawwadchandio"
+      github_user  = "SimonGivan"
       permission   = "admin"
-      name         = "Jawwad Chandio"                                                                      #  The name of the person behind github_user
-      email        = "jawwad.chandio@kainos.com"                                                           #  Their email address
-      org          = "Kainos"                                                                              #  The organisation/entity they belong to
-      reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo" #  Why is this person being granted access?
-      added_by     = "Incell Infrastructure team, federico.staiano1@justice.gov.uk"                        #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-06-18"                                                                          #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
-      github_user  = "muhammadhusain"
-      permission   = "admin"
-      name         = "Muhammad Husain"                                                                     #  The name of the person behind github_user
-      email        = "muhammad.husain@kainos.com"                                                          #  Their email address
-      org          = "Kainos"                                                                              #  The organisation/entity they belong to
-      reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo" #  Why is this person being granted access?
-      added_by     = "Incell Infrastructure team, federico.staiano1@justice.gov.uk"                        #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-06-18"                                                                          #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
-      github_user  = "lukecalderon-kainos"
-      permission   = "admin"
-      name         = "Luke Calderon"                                                                       # The name of the person
-      email        = "luke.calderon@kainos.com"                                                            # Email
+      name         = "Simon Givan"                                                                       # The name of the person
+      email        = "s.givan@kainos.com"                                                            # Email
       org          = "Kainos"                                                                              #  The organisation/entity they belong to
       reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo" #  Why is this person being granted access?
       added_by     = "Incell Infrastructure team, federico.staiano1@justice.gov.uk"                        #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
