be0a6202079049b29de6d6dbbc58c8665eee842d
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-03-25 10:00:45 +0000

    Update yjaf-transitions.tf (#570)

--- terraform/yjaf-transitions.tf
+++ terraform/yjaf-transitions.tf
@@ -42,5 +42,25 @@ module "yjaf-transitions" {
       added_by     = "Jon Dent jon.dent@justice.gov.uk"
       review_after = "2023-03-07"
     },
+    {
+      github_user  = "ttipler"
+      permission   = "admin"
+      name         = "Thomas Tipler"
+      email        = "thomas.tipler@necsws.com"
+      org          = "NEC SWS"
+      reason       = "Working on pipeline for deployment of microservice"
+      added_by     = "Jon Dent jon.dent@justice.gov.uk"
+      review_after = "2023-03-24"
+    },
+    {
+      github_user  = "gregi2n"
+      permission   = "admin"
+      name         = "Gregory Whiting"
+      email        = "greg.whiting@necsws.com"
+      org          = "NEC SWS"
+      reason       = "Working on pipeline for deployment of microservice"
+      added_by     = "Jon Dent jon.dent@justice.gov.uk"
+      review_after = "2023-03-24"
+    },
   ]
 }
