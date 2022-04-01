aed5e5ca9cca98a85aaf0068bb5e7ffe2725bc9e
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-04-01 15:41:19 +0100

    Add Toby Liddicoat to hmpps-vcms repo

--- terraform/hmpps-vcms.tf
+++ terraform/hmpps-vcms.tf
@@ -42,5 +42,15 @@ module "hmpps-vcms" {
       added_by     = "Probation Infrastructure AWS Team, maximillian.lakanu@digital.justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-05-21"                                                                   #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
+    {
+      github_user  = "codesureuk"
+      permission   = "maintain"
+      name         = "Toby Liddicoat"                                                               #  The name of the person behind github_user
+      email        = "toby.liddicoat@civica.co.uk"                                                  #  Their email address
+      org          = "Civica"                                                                       #  The organisation/entity they belong to
+      reason       = "Civica developer for Victims Case Management System"                          #  Why is this person being granted access?
+      added_by     = "Probation Infrastructure AWS Team, maximillian.lakanu@digital.justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2023-03-31"                                                                   #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
   ]
 }
