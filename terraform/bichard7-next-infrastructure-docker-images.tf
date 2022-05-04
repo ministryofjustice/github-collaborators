e97dd974049883abc4e7d95a2ab2e5ba8c198ac0
Ben Pirt <ben@pirt.co.uk>
2022-05-04 11:03:48 +0100

    Add a new dev to the Bichard repos

--- terraform/bichard7-next-infrastructure-docker-images.tf
+++ terraform/bichard7-next-infrastructure-docker-images.tf
@@ -81,6 +81,16 @@ module "bichard7-next-infrastructure-docker-images" {
       reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
       added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "joe-fol"
+      permission   = "push"
+      name         = "Joe Folkard"                              #  The name of the person behind github_user
+      email        = "joe.folkard@madetech.com"                 #  Their email address
+      org          = "Madetech"                                 #  The organisation/entity they belong to
+      reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
+      added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     }
   ]
 }
