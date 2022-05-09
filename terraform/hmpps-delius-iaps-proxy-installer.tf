c6875af780344e6cb210b78bed17614c5e809fe1
jakemulley <jakemulley@users.noreply.github.com>
2022-05-09 08:42:15 +0100

    Remove Stuart Westbrook

--- terraform/hmpps-delius-iaps-proxy-installer.tf
+++ terraform/hmpps-delius-iaps-proxy-installer.tf
@@ -2,15 +2,6 @@ module "hmpps-delius-iaps-proxy-installer" {
   source     = "./modules/repository-collaborators"
   repository = "hmpps-delius-iaps-proxy-installer"
   collaborators = [
-    {
-      github_user  = "swestb"
-      permission   = "admin"
-      name         = "Stuart Westbrook"
-      email        = "stuart.westbrook@adrocgroup.com"
-      org          = "Adroc Group"
-      reason       = "In support of migration activities" #  Why is this person being granted access?
-      added_by     = "maximillian.lakanu@digital.justice.gov.uk"
-      review_after = "2022-06-17"
-    },
+
   ]
 }
