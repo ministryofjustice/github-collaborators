cb667fcfa5139374f09f902ddf715eac30164953
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-05-12 14:43:26 +0100

    Remove collaborator

--- terraform/YJAF-POC.tf
+++ terraform/YJAF-POC.tf
@@ -13,16 +13,6 @@ module "YJAF-POC" {
       review_after = "2022-12-13"
     },
     {
-      github_user  = "brbaje-dev"
-      permission   = "admin"
-      name         = "Ben Bajek"
-      email        = "ben.bajek@necsws.com"
-      org          = "NEC Software Solutions"
-      reason       = "Devops guys need access to make app/infra changes"
-      added_by     = "Greg Whiting - greg.whiting@northgateps.com"
-      review_after = "2022-07-31"
-    },
-    {
       github_user  = "ttipler"
       permission   = "admin"
       name         = "Thomas Tipler"
