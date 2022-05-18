0a0a19316e002866af89e7e188673be7a96fbf82
seanprivett <sean.privett@digital.justice.gov.uk>
2022-05-18 10:28:44 +0100

    Update hmpps-vcms.tf
    Added Ahmed Alam, a new Civica developer working on the VCMS project.  Same permissions as Miriam and Toby

--- terraform/hmpps-vcms.tf
+++ terraform/hmpps-vcms.tf
@@ -14,6 +14,16 @@ module "hmpps-vcms" {
       review_after = "2022-11-21"                                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
+      github_user  = "ahmedalam-civica"
+      permission   = "maintain"
+      name         = "Ahmed Alam"                                                                      #  The name of the person behind github_user
+      email        = "ahmed.alam@civica.co.uk"                                                         #  Their email address
+      org          = "Civica"                                                                            #  The organisation/entity they belong to
+      reason       = "Civica developer that helps the development of the Victims Case Management System" #  Why is this person being granted access?
+      added_by     = "Probation Infrastructure AWS Team, awssupportteam@digital.justice.gov.uk"          #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-11-21"                                                                        #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
       github_user  = "miriamgo-civica"
       permission   = "maintain"
       name         = "Miriam Gomez-Orozco"                                                          #  The name of the person behind github_user
