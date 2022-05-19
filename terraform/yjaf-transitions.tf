6934ad34a89da16a5d060b9579d7bfb3cf9dde9b
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-05-19 14:43:00 +0100

    Add new collaborator

--- terraform/yjaf-transitions.tf
+++ terraform/yjaf-transitions.tf
@@ -62,5 +62,15 @@ module "yjaf-transitions" {
       added_by     = "Jon Dent jon.dent@justice.gov.uk"
       review_after = "2023-03-24"
     },
+    {
+      github_user  = "craig-ashford"
+      permission   = "maintain"
+      name         = "Craig Ashford"
+      email        = "craig.ashford@necsws.com"
+      org          = "NEC SWS"
+      reason       = "New front end developer on YJAF team"
+      added_by     = "Jon Dent jon.dent@justice.gov.uk"
+      review_after = "2022-05-19"
+    },
   ]
 }
