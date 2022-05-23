0387ceec2adca2d7ae32fa211b6cc28a32a77acb
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-05-23 10:48:31 +0100

    Update review_after

--- terraform/yjaf-infra-aws-mgmt.tf
+++ terraform/yjaf-infra-aws-mgmt.tf
@@ -20,7 +20,7 @@ module "yjaf-infra-aws-mgmt" {
       org          = "Youth Justice Board"
       reason       = "Technical Assurance Architect (managed service)"
       added_by     = "Jake Mulley (MOJ)"
-      review_after = "2022-06-18"
+      review_after = "2022-12-18"
     },
     {
       github_user  = "oliviergaubert"
