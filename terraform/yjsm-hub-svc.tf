3bf7cf708521cf148f53abfa4f91d929c1c54d57
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-05-27 16:15:54 +0100

    add user

--- terraform/yjsm-hub-svc.tf
+++ terraform/yjsm-hub-svc.tf
@@ -121,6 +121,16 @@ module "yjsm-hub-svc" {
       reason       = "YJAF developer. Needing access to the work on backend coding tasks"
       added_by     = "<operations-engineering@digital.justice.gov.uk> on behalf of the YJB"
       review_after = "2022-12-02"
-    }
+    },
+    {
+      github_user  = "AndrewTRichards"
+      permission   = "admin"
+      name         = "Andrew Richards"
+      email        = "a.t.richards@btopenworld.com"
+      org          = "NEC"
+      reason       = "New starter (well returning Dev) working on all YJAF projects etc"
+      added_by     = "Jon Dent jon.dent@justice.gov.uk"
+      review_after = "2023-05-26"
+    },
   ]
 }
