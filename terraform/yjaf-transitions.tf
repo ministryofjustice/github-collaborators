a8273729af8ebe6517dda9a31ab569c65ca3e280
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-05-27 14:37:46 +0100

    correct mistake

--- terraform/yjaf-transitions.tf
+++ terraform/yjaf-transitions.tf
@@ -73,6 +73,16 @@ module "yjaf-transitions" {
       review_after = "2022-11-19"
     },
     {
+      github_user  = "djv72"
+      permission   = "push"
+      name         = "David Vincent"
+      email        = "david.vincent@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
+      added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
+      review_after = "2022-11-19"
+    },
+    {
       github_user  = "AndrewTRichards"
       permission   = "admin"
       name         = "Andrew Richards"
