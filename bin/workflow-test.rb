77c1f3c9a28cd81a67c2a327bf705cd2591049fa
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-06-23 17:39:26 +0100

    Delete workflow-test.rb

--- bin/workflow-test.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative "../lib/github_collaborators"
-
-puts "This is a Ruby file for the GitHub action/workflow workflow-test.yml"
-puts "Please change this file on a branch called workflow_test to test GitHub actions on branch (non-main)"
