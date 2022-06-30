ceb26f6dec9ab10d676b0fa074cada5cd9c5a22b
Ben Ashton <61050986+ben-al@users.noreply.github.com>
2022-06-30 10:54:05 +0100

    Simplify pattern recognition (#659)
    * Test test
    
    * Simplify pattern recognition
    
    * Delete old file

--- scripts/permission-check.sh
+++ scripts/permission-check.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -z $(grep permission *.tf | awk '{ print $4 }' | sed 's/"//g' | awk -e '$NF !~ /admin/ || /pull/ || /triage/ || /push/ || /maintain/') ]
+if [[ -z $(grep permission terraform/*.tf | sed 's/"//g' | awk '$4 !~ /admin|push|maintain|pull|triage/') ]]
 then
     exit 0
 else
+    echo ERROR: Permission must be Admin, Push, Maintain, Pull or Triage
     exit 1
 fi
