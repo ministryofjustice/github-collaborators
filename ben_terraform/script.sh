f67e4eb34d0451e7151f8aae06ac32953b6c72b5
Ben Ashton <ben.ashton@digital.justice.gov.uk>
2022-06-30 10:46:29 +0100

    Delete old file

--- ben_terraform/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z $(grep permission *.tf | awk '{ print $4 }' | sed 's/"//g' | awk -e '$NF !~ /admin/ || /pull/ || /triage/ || /push/ || /maintain/') ]
-then
-    exit 0
-else
-    exit 1
-fi
