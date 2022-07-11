f56d56daa99d8e4f25a18b19088b15349d4c4b5a
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-07-11 10:44:16 +0100

    Reinstate and remove a collaborator

--- terraform/yjaf-nginx-gateway.tf
+++ terraform/yjaf-nginx-gateway.tf
@@ -13,16 +13,6 @@ module "yjaf-nginx-gateway" {
       review_after = "2022-12-13"
     },
     {
-      github_user  = "djv72"
-      permission   = "push"
-      name         = "David Vincent"
-      email        = "david.vincent@necsws.com"
-      org          = "NEC Software Solutions"
-      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
-      review_after = "2022-12-18"
-    },
-    {
       github_user  = "InFlamesForever"
       permission   = "admin"
       name         = "Richard Came"
@@ -101,6 +91,16 @@ module "yjaf-nginx-gateway" {
       reason       = "New starter (well returning Dev) working on all YJAF projects etc"
       added_by     = "Jon Dent jon.dent@justice.gov.uk"
       review_after = "2023-05-26"
+    },
+    {
+      github_user  = "VasilDimitrov22"
+      permission   = "push"
+      name         = "Vasil Dimitrov"
+      email        = "vasil.dimitrov@northgateps.com"
+      org          = "NPS (northgate)"
+      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
+      added_by     = "Greg Whiting <greg.whiting@northgateps.com> Devops for northgate"
+      review_after = "2022-12-18"
     },
   ]
 }
