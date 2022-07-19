1671ac7c9acf64625dc2dd0497a60ccd29dc21f1
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-07-19 17:13:24 +0100

    Revert add parrokajadm (#679)
    Wrong username provided

--- terraform/modernisation-platform-environments.tf
+++ terraform/modernisation-platform-environments.tf
@@ -63,16 +63,6 @@ module "modernisation-platform-environments" {
       review_after = "2022-11-01"                                                                                  #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
     {
-      github_user  = "parrokajadm"
-      permission   = "push"
-      name         = "Joy Parokkaran"                                                                              #  The name of the person behind github_user
-      email        = "Joy.Parokkaran@roctechnologies.com"                                                          #  Their email address
-      org          = "Roc Technologies"                                                                            #  The organisation/entity they belong to
-      reason       = "Roc have built and are supporting the HMPPS Equip application on the Modernisation Platform" #  Why is this person being granted access?
-      added_by     = "Modernisation Platform team, modernisation-platform@digital.justice.gov.uk"                  #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-11-01"                                                                                  #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
       github_user  = "trudley"
       permission   = "push"
       name         = "Tom Rudley"                                                                                  #  The name of the person behind github_user
