f101360862f2e4385b838da588efc2c9b8ac37ab
MatthewWhiteMoJ <61458526+MatthewWhiteMoJ@users.noreply.github.com>
2022-07-27 14:18:50 +0100

    Add new MT devs and amend C+C review date

--- terraform/staff-device-management-windows10-mass-storage-controls.tf
+++ terraform/staff-device-management-windows10-mass-storage-controls.tf
@@ -10,6 +10,26 @@ module "staff-device-management-windows10-mass-storage-controls" {
       org          = "Content and Cloud"               #  The organisation/entity they belong to
       reason       = "PTTP Tech team"                  #  Why is this person being granted access?
       added_by     = "matthew.white1@justice.gov.uk"   #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-08-01"                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "JazJax"
+      permission   = "admin"
+      name         = "Jasper Jackson"                     #  The name of the person behind github_user
+      email        = "jasper.jackson@madetech.com" #  Their email address
+      org          = "MadeTech"               #  The organisation/entity they belong to
+      reason       = "VICTOR product development"                  #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk"   #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    },
+    {
+      github_user  = "BingliuMT"
+      permission   = "admin"
+      name         = "Bingjie Liu"                     #  The name of the person behind github_user
+      email        = "bingjie.liu@madetech.com" #  Their email address
+      org          = "MadeTech"               #  The organisation/entity they belong to
+      reason       = "VICTOR product development"                  #  Why is this person being granted access?
+      added_by     = "matthew.white1@justice.gov.uk"   #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
       review_after = "2022-12-31"                      #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     }
   ]
