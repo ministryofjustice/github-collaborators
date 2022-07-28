f6bedeba036878fb39a76b867464a59b295905cd
Jake Mulley <jakemulley@users.noreply.github.com>
2022-07-28 13:10:53 +0100

    Give Tim Jacomb access to c100

---
+++ terraform/c100-application-deploy.tf
@@ -0,0 +1,16 @@
+module "c100-application-deploy" {
+  source     = "./modules/repository-collaborators"
+  repository = "c100-application-deploy"
+  collaborators = [
+    {
+      github_user  = "timja"
+      permission   = "write"
+      name         = "Tim Jacomb"                         # The name of the person behind github_user
+      email        = "tim.jacomb@hmcts.net"               # Their email address
+      org          = "HMCTS"                              # The organisation/entity they belong to
+      reason       = "HMCTS migration team"               # Why is this person being granted access?
+      added_by     = "jake.mulley@digital.justice.gov.uk" # Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
+      review_after = "2022-12-31"                         # Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+    }
+  ]
+}
