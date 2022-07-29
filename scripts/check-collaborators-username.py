b9ab63e3c0a23f3db8a8ce7ab7830ea18ef54a00
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-07-29 14:59:00 +0100

    Update check-collaborators-username.py

--- scripts/check-collaborators-username.py
+++ scripts/check-collaborators-username.py
@@ -7,7 +7,7 @@ if len(sys.argv) == 2:
     oauth_token = sys.argv[1]
 else:
     print("Missing a script input parameter")
-    sys.exit()
+    sys.exit(1)
 
 
 def get_collaborator_names() -> list:
