d9cb4b1392eeefdb0120453af2a37ea792ee3ed2
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-08-23 10:36:02 +0100

    remove hullcoastie

--- terraform/modernisation-platform-environments.tf
+++ terraform/modernisation-platform-environments.tf
@@ -3,16 +3,6 @@ module "modernisation-platform-environments" {
   repository = "modernisation-platform-environments"
   collaborators = [
     {
-      github_user  = "hullcoastie"
-      permission   = "push"
-      name         = "John Broom"                                                                                  #  The name of the person behind github_user
-      email        = "John.Broom@roctechnologies.com"                                                              #  Their email address
-      org          = "Roc Technologies"                                                                            #  The organisation/entity they belong to
-      reason       = "Roc have built and are supporting the HMPPS Equip application on the Modernisation Platform" #  Why is this person being granted access?
-      added_by     = "Modernisation Platform team, modernisation-platform@digital.justice.gov.uk"                  #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-11-01"                                                                                  #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
-    },
-    {
       github_user  = "jamesashton-roc"
       permission   = "push"
       name         = "James Ashton"                                                                                #  The name of the person behind github_user
