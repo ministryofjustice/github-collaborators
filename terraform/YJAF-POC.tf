b844b741459dea09617a8a564328d04c1cf4a739
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-08-24 16:02:49 +0100

    Remove InFlamesForever and extend a review_after

--- terraform/YJAF-POC.tf
+++ terraform/YJAF-POC.tf
@@ -20,7 +20,7 @@ module "YJAF-POC" {
       org          = "NEC Software Solutions"
       reason       = "Devops guys need access to make app/infra changes"
       added_by     = "Greg Whiting - greg.whiting@northgateps.com"
-      review_after = "2022-07-31"
+      review_after = "2023-02-24"
     },
     {
       github_user  = "jondent"
@@ -41,16 +41,6 @@ module "YJAF-POC" {
       reason       = "Part of the Northgate supplier team who are now NEC for the YJB YJAF system"
       added_by     = "Jon Dent jon.dent@justice.gov.uk"
       review_after = "2022-12-31"
-    },
-    {
-      github_user  = "InFlamesForever"
-      permission   = "admin"
-      name         = "Richard Came"
-      email        = "richard.came@necsws.com"
-      org          = "NEC Software Solutions"
-      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = "Jon Dent jon.dent@justice.gov.uk"
-      review_after = "2022-08-27"
     },
     {
       github_user  = "AndrewTRichards"
