d1b909eeb01b1393a0090f76f156b5709069b60a
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-08-24 16:18:20 +0100

    Remove InFlamesForever and extend a review_after

--- terraform/yjaf-nginx-gateway.tf
+++ terraform/yjaf-nginx-gateway.tf
@@ -13,16 +13,6 @@ module "yjaf-nginx-gateway" {
       review_after = "2022-12-13"
     },
     {
-      github_user  = "InFlamesForever"
-      permission   = "admin"
-      name         = "Richard Came"
-      email        = "richard.came@necsws.com"
-      org          = "NEC Software Solutions"
-      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk"
-      review_after = "2022-08-27"
-    },
-    {
       github_user  = "ttipler"
       permission   = "admin"
       name         = "Thomas Tipler"
@@ -30,7 +20,7 @@ module "yjaf-nginx-gateway" {
       org          = "NEC Software Solutions"
       reason       = "Devops guys need access to make app/infra changes"
       added_by     = "Greg Whiting - greg.whiting@northgateps.com"
-      review_after = "2022-07-31"
+      review_after = "2023-02-24"
     },
     {
       github_user  = "jondent"
