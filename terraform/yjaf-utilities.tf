fd7af2d926eaef5a8be128c18be07deec7c1d811
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-08-24 16:11:10 +0100

    Remove collaborator

--- terraform/yjaf-utilities.tf
+++ terraform/yjaf-utilities.tf
@@ -73,16 +73,6 @@ module "yjaf-utilities" {
       review_after = "2023-02-11"
     },
     {
-      github_user  = "InFlamesForever"
-      permission   = "admin"
-      name         = "Richard Came"
-      email        = "richard.came@necsws.com"
-      org          = "NEC Software Solutions"
-      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = "Jon Dent jon.dent@justice.gov.uk"
-      review_after = "2022-08-27"
-    },
-    {
       github_user  = "AndrewTRichards"
       permission   = "admin"
       name         = "Andrew Richards"
