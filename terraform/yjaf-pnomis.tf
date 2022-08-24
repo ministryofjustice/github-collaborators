ef8407383b9f4524274b4cd611de26e3e6d76d97
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-08-24 16:16:49 +0100

    Remove collaborator

--- terraform/yjaf-pnomis.tf
+++ terraform/yjaf-pnomis.tf
@@ -53,16 +53,6 @@ module "yjaf-pnomis" {
       review_after = "2022-12-31"
     },
     {
-      github_user  = "InFlamesForever"
-      permission   = "admin"
-      name         = "Richard Came"
-      email        = "richard.came@necsws.com"
-      org          = "NEC Software Solutions"
-      reason       = "YJAF developer. Needing access to the work on BackEnd coding Tasks"
-      added_by     = "Jon Dent jon.dent@justice.gov.uk"
-      review_after = "2022-08-27"
-    },
-    {
       github_user  = "AndrewTRichards"
       permission   = "admin"
       name         = "Andrew Richards"
