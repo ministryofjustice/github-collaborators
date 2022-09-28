1fa69cb7675e59c8782d2c7f4b09b5a8abba932a
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-09-28 11:10:46 +0100

    Delete post-data.sh

--- bin/post-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-bin/outside-collaborators.rb > data.json
-
-curl -vvv \
-  -H "Content-Type: application/json" \
-  -H "X-API-KEY: ${OPERATIONS_ENGINEERING_REPORTS_API_KEY}" \
-  -d @data.json \
-  ${OPERATIONS_ENGINEERING_REPORTS_HOST}/github_collaborators
