866f79cfa851476eadd00ee102cf786333f27934
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-09-28 16:09:02 +0100

    Remove collaborator

--- terraform/yjaf-case.tf
+++ terraform/yjaf-case.tf
@@ -13,16 +13,6 @@ module "yjaf-case" {
       review_after = "2022-12-13"
     },
     {
-      github_user  = "TomDover-NorthgatePS"
-      permission   = "admin"
-      name         = "Tom Dover"
-      email        = "tom.dover@northgateps.com"
-      org          = "Northgate"
-      reason       = "Part of the Northgate supplier team for the YJB YJAF system"
-      added_by     = "<gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
-      review_after = "2022-12-13"
-    },
-    {
       github_user  = "VasilDimitrov22"
       permission   = "push"
       name         = "Vasil Dimitrov"
