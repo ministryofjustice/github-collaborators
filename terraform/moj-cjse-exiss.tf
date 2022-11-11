e294c7f49276220f4bf6411550541c536aebafcc
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-11-11 12:56:03 +0000

    Extend review date in Terraform file/s for bjpirt

--- terraform/moj-cjse-exiss.tf
+++ terraform/moj-cjse-exiss.tf
@@ -10,7 +10,7 @@ module "moj-cjse-exiss" {
       org          = "Madetech"                                 #  The organisation/entity they belong to
       reason       = "CJSE Bichard Development"                 #  Why is this person being granted access?
       added_by     = "Dom Tomkins <dom.tomkins@justice.gov.uk>" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2022-06-17"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2022-12-17"                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
 }
