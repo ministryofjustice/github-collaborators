33fa38ddd41184efa010f7a57b08e4358783f932
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-11-16 14:29:55 +0000

    Fix date (#815)

--- terraform/hmpps-domestic-abuse-support-officers.tf
+++ terraform/hmpps-domestic-abuse-support-officers.tf
@@ -60,7 +60,7 @@ module "hmpps-domestic-abuse-support-officers" {
       org          = "Civica"                                                                   #  The organisation/entity they belong to
       reason       = "Civica developer for DASO"                                                #  Why is this person being granted access?
       added_by     = "Probation Infrastructure AWS Team, awssupportteam@digital.justice.gov.uk" #  Who made the decision to grant them access? e.g. 'Awesome Team <awesome.team@digital.justice.gov.uk>'
-      review_after = "2023-04-31"                                                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
+      review_after = "2023-04-30"                                                               #  Date after which this grant should be reviewed/revoked, e.g. 2021-11-26
     },
   ]
 }
