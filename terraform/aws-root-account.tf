3397c211c508fb7d4080f02a38bf2302e092a475
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-11-23 10:51:14 +0000

    Fix typo

--- terraform/aws-root-account.tf
+++ terraform/aws-root-account.tf
@@ -21,6 +21,6 @@ module "aws-root-account" {
       reason       = "Full Org member / collaborator missing from Terraform file"
       added_by     = "opseng-bot@digital.justice.gov.uk"
       review_after = "2023-02-20"
-    {
+    },
   ]
 }
