30597580604f97b56abbb7099c8c638269f95628
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-11-24 13:09:40 +0000

    fix typo (#961)

--- lib/github_collaborators/issue_creator.rb
+++ lib/github_collaborators/issue_creator.rb
@@ -51,7 +51,7 @@ class GithubCollaborators
       # This is a work around for when collaborators unassign themself from the ticket without updating their review_after
       # There is a better way to reassign them but would involve some fairly big code edits, this closes the unassigned ticket and makes a new one
       bad_issues = issues.select { |issue| issue[:assignees].length == 0 }
-      bad_issues.each { |issue| GithubCollaborators::IssueClose.remove_issue(@repository, issue[:number]) }
+      bad_issues.each { |issue| GithubCollaborators::IssueClose.new.remove_issue(@repository, issue[:number]) }
       issues.delete_if { |issue| issue[:assignees].length == 0 }
 
       # Check which issues are assigned to the collaborator
