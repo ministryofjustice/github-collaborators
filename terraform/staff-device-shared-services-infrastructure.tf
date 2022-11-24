f2a8bded75df23e13d6fb384a2c4b1f24a428d5b
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-11-24 13:24:45 +0000

    Remove collaborators

--- terraform/staff-device-shared-services-infrastructure.tf
+++ terraform/staff-device-shared-services-infrastructure.tf
@@ -3,36 +3,6 @@ module "staff-device-shared-services-infrastructure" {
   repository = "staff-device-shared-services-infrastructure"
   collaborators = [
     {
-      github_user  = "yusufsheiqh"
-      permission   = "admin"
-      name         = "Yusuf Sheikh"
-      email        = "yusuf@madetech.com"
-      org          = "Made Tech Ltd"
-      reason       = "MoJ Network Access Control Tech Team"
-      added_by     = "justin.fielding@justice.gov.uk"
-      review_after = "2022-01-01"
-    },
-    {
-      github_user  = "MichaelCullenMadeTech"
-      permission   = "admin"
-      name         = "Michael Cullen"
-      email        = "michael.cullen@madetech.com"
-      org          = "Made Tech Ltd"
-      reason       = "MoJ Network Access Control Tech Team"
-      added_by     = "justin.fielding@justice.gov.uk"
-      review_after = "2022-01-01"
-    },
-    {
-      github_user  = "C-gyorfi"
-      permission   = "admin"
-      name         = "Csaba Gyorfi"
-      email        = "csaba@madetech.com"
-      org          = "Made Tech Ltd"
-      reason       = "MoJ Network Access Control Tech Team"
-      added_by     = "justin.fielding@justice.gov.uk"
-      review_after = "2022-01-01"
-    },
-    {
       github_user  = "emileswarts"
       permission   = "admin"
       name         = "Emile Swarts"
