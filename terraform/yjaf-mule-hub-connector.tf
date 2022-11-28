5e4510854c01aecd795c4f0b32b2b9f0808e3f33
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-11-28 11:43:44 +0000

    Update collaborator user name

--- terraform/yjaf-mule-hub-connector.tf
+++ terraform/yjaf-mule-hub-connector.tf
@@ -43,7 +43,7 @@ module "yjaf-mule-hub-connector" {
       review_after = "2022-12-12"
     },
     {
-      github_user  = "craig-ashford"
+      github_user  = "craig-ashford-nec"
       permission   = "maintain"
       name         = "Craig Ashford"
       email        = "craig.ashford@necsws.com"
