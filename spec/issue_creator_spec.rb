73f1f2197114bee3d6eb526c9b9f2e97102d274f
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-11-30 16:05:55 +0000

    issue creator test

--- spec/issue_creator_spec.rb
+++ spec/issue_creator_spec.rb
@@ -46,28 +46,23 @@ class GithubCollaborators
         expect(http_client).not_to receive(:post_json)
         issue_creator.create_review_date_expires_soon_issue
       end
+      
+      let(:json2) {
+        %({"title":"Collaborator review date expires soon for user somegithubuser","assignees":["somegithubuser"],"body":"Hi there\\n\\nThe user @somegithubuser has its access for this repository maintained in code here: https://github.com/ministryofjustice/github-collaborators\\n\\nThe review_after date is due to expire within one month, please update this via a PR if they still require access.\\n\\nIf you have any questions, please post in #ask-operations-engineering on Slack.\\n\\nFailure to update the review_date will result in the collaborator being removed from the repository via our automation.\\n"})
+      }
 
-      it "delete an issue when issue missing an assignee" do
+      it "delete an issue when issue missing an assignee then create an issue" do
         response = [{number: 3, assignee: {login: "somegithubuser"}, title: "Collaborator review date expires soon for user somegithubuser", assignees: []}]
         expect(HttpClient).to receive(:new).and_return(http_client).at_least(3).times
 
         close_issue_url = URL + "/3"
         state = {state: "closed"}
-        expect(http_client).to receive(:patch_json).with(close_issue_url, state.to_json)
         expect(http_client).to receive(:fetch_json).with(URL).and_return(response.to_json)
+        expect(http_client).to receive(:patch_json).with(close_issue_url, state.to_json)
         expect(http_client).to receive(:post_json).with(URL, json2)
         issue_creator.create_review_date_expires_soon_issue
       end
 
-      # it "in create review date expires soon issue type two" do
-      #   response = '[{"assignee": { "login": "somegithubuser" }, "title": "Review after date expiry is upcoming for user", "assignees": [{"login":"somegithubuser"}]}]'
-
-      # end
-
-      let(:json2) {
-        %({"title":"Collaborator review date expires soon for user somegithubuser","assignees":["somegithubuser"],"body":"Hi there\\n\\nThe user @somegithubuser has its access for this repository maintained in code here: https://github.com/ministryofjustice/github-collaborators\\n\\nThe review_after date is due to expire within one month, please update this via a PR if they still require access.\\n\\nIf you have any questions, please post in #ask-operations-engineering on Slack.\\n\\nFailure to update the review_date will result in the collaborator being removed from the repository via our automation.\\n"})
-      }
-      # .to receive(:post_json).with(url, json2)
       after do
         ENV["REALLY_POST_TO_GH"] = "0"
       end
@@ -94,6 +89,14 @@ class GithubCollaborators
         expect(http_client).not_to receive(:delete)
         issue_creator.create_unknown_collaborator_issue
       end
+
+      it "dont call github api in create review date expires soon issue" do
+        expect(HttpClient).to receive(:new).and_return(http_client)
+        response = []
+        expect(http_client).to receive(:fetch_json).with(URL).and_return(response.to_json)
+        expect(http_client).not_to receive(:delete)
+        issue_creator.create_review_date_expires_soon_issue
+      end
     end
 
     context "call get issues" do
@@ -116,44 +119,11 @@ class GithubCollaborators
       end
 
       it "returns [] if no issues" do
-        response = "[]"
+        response = []
         expect(HttpClient).to receive(:new).and_return(http_client)
         expect(http_client).to receive(:fetch_json).with(URL).and_return(response.to_json)
         expect(issue_creator.get_issues("somerepo")).equal?([])
       end
     end
-
-    # it "close issue three" do
-    #   response = Net::HTTPSuccess.new(1.0, "200", "OK")
-    #   expect(response).to receive(:body) { '[{ "number": 3, "created_at": "2022-03-12T04:23:22Z", "state": "open", "assignee": { "login": "somegithubuser" }, "title": "Please define outside collaborators in code", "assignees": [{"login":"somegithubuser"}]} ]' }
-    #   expect(HttpClient).to receive(:new).and_return(http_client)
-    #   expect(http_client).to receive(:fetch_json).and_return(response)
-    #   allow(ic).to receive(:remove_issue).with(3).and_return("")
-    #   allow_any_instance_of(IssueCreator).to receive_message_chain(:sleep)
-    #   ic.close_expired_issues
-    # end
-
-    # it "close issue not yet" do
-    #   response = Net::HTTPSuccess.new(1.0, "200", "OK")
-    #   expect(response).to receive(:body) { '[{ "number": 3, "created_at": "2011-01-01T04:23:22Z", "state": "open", "assignee": { "login": "somegithubuser" }, "title": "Collaborator review date expires soon", "assignees": [{"login":"somegithubuser"}]} ]' }
-    #   expect(HttpClient).to receive(:new).and_return(http_client)
-    #   expect(http_client).to receive(:fetch_json).and_return(response)
-    #   stub_date = Date.parse("2011-02-15")
-    #   allow(Date).to receive(:today).and_return(stub_date)
-    #   allow_any_instance_of(IssueCreator).to receive_message_chain(:sleep)
-    #   ic.close_expired_issues
-    # end
-
-    # it "close issue now" do
-    #   response = Net::HTTPSuccess.new(1.0, "200", "OK")
-    #   expect(response).to receive(:body) { '[{ "number": 3, "created_at": "2011-01-01T04:23:22Z", "state": "open", "assignee": { "login": "somegithubuser" }, "title": "Collaborator review date expires soon", "assignees": [{"login":"somegithubuser"}]} ]' }
-    #   expect(HttpClient).to receive(:new).and_return(http_client)
-    #   expect(http_client).to receive(:fetch_json).and_return(response)
-    #   stub_date = Date.parse("2011-02-16")
-    #   allow(Date).to receive(:today).and_return(stub_date)
-    #   allow(ic).to receive(:remove_issue).with(3).and_return("")
-    #   allow_any_instance_of(IssueCreator).to receive_message_chain(:sleep)
-    #   ic.close_expired_issues
-    # end
   end
 end
