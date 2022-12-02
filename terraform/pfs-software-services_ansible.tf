9c59413fb24e6350f9f7f239d2be1b0f7a73f700
federicostaianojustice1 <86769850+federicostaianojustice1@users.noreply.github.com>
2022-12-02 15:23:20 +0000

    Update pfs-software-services_ansible.tf
    Kainos collaborators extension for code reviewing

--- terraform/pfs-software-services_ansible.tf
+++ terraform/pfs-software-services_ansible.tf
@@ -10,7 +10,7 @@ module "pfs-software-services_ansible" {
       org          = "Kainos"
       reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo for Jenkins"
       added_by     = "Incell Infrastructure team, federico.staiano1@justice.gov.uk"
-      review_after = "2022-12-18"
+      review_after = "2023-05-18"
     },
     {
       github_user  = "simongivan"
@@ -20,7 +20,7 @@ module "pfs-software-services_ansible" {
       org          = "Kainos"
       reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo"
       added_by     = "Incell Infrastructure team, federico.staiano1@justice.gov.uk"
-      review_after = "2022-12-18"
+      review_after = "2023-05-18"
     },
   ]
 }
