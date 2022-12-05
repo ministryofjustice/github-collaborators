1985442c088ba4359f2ffd0d19e19efce6a3c6d7
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-12-05 17:13:17 +0000

    add negative tests

--- spec/helper_module_spec.rb
+++ spec/helper_module_spec.rb
@@ -55,12 +55,20 @@ describe HelperModule do
 
   context "test get_repository_invites" do
     let(:json) { File.read("spec/fixtures/invites.json") }
-    it "call function" do
+    it "when invites exist" do
       url = "https://api.github.com/repos/ministryofjustice/somerepo/invitations"
       expect(GithubCollaborators::HttpClient).to receive(:new).and_return(http_client)
       expect(http_client).to receive(:fetch_json).with(url).and_return(json)
       expected_result = [{login: "user1", expired: true, invite_id: 1}, {login: "user2", expired: false, invite_id: 2}]
       expect(helper_module.get_repository_invites("somerepo")).to eq(expected_result)
+    end
+
+    it "when invites don't exist" do
+      url = "https://api.github.com/repos/ministryofjustice/somerepo/invitations"
+      response = %([])
+      expect(GithubCollaborators::HttpClient).to receive(:new).and_return(http_client)
+      expect(http_client).to receive(:fetch_json).with(url).and_return(response)
+      expect(helper_module.get_repository_invites("somerepo")).to eq([])
     end
   end
 
@@ -82,13 +90,21 @@ describe HelperModule do
   end
 
   context "test get_all_org_members_team_repositories" do
-    it "call function" do
-      url = "https://api.github.com/orgs/ministryofjustice/teams/all-org-members/repos?per_page=100"
+    url = "https://api.github.com/orgs/ministryofjustice/teams/all-org-members/repos?per_page=100"
+    
+    it "when team has repositories" do
       response = %([{"name": "somerepo1"},{"name": "somerepo2"}])
       expect(GithubCollaborators::HttpClient).to receive(:new).and_return(http_client)
       expect(http_client).to receive(:fetch_json).with(url).and_return(response)
       repositories = ["somerepo1", "somerepo2"]
       expect(helper_module.get_all_org_members_team_repositories).to eq(repositories)
+    end
+
+    it "when team has no repositories" do
+      response = []
+      expect(GithubCollaborators::HttpClient).to receive(:new).and_return(http_client)
+      expect(http_client).to receive(:fetch_json).with(url).and_return(response.to_json)
+      expect(helper_module.get_all_org_members_team_repositories).to eq([])
     end
   end
 
@@ -133,13 +149,21 @@ describe HelperModule do
   end
 
   context "test get_org_outside_collaborators" do
-    it "call get_org_outside_collaborators" do
-      url = "https://api.github.com/orgs/ministryofjustice/outside_collaborators?per_page=100"
+    url = "https://api.github.com/orgs/ministryofjustice/outside_collaborators?per_page=100"
+    
+    it "when collaborators exist" do
       json = %([{"login": "someuser1"},{"login": "someuser2"}])
       response = ["someuser1", "someuser2"]
       expect(GithubCollaborators::HttpClient).to receive(:new).and_return(http_client)
       expect(http_client).to receive(:fetch_json).with(url).and_return(json)
       expect(helper_module.get_org_outside_collaborators).to eq(response)
+    end
+
+    it "when no collaborators exist" do
+      response = []
+      expect(GithubCollaborators::HttpClient).to receive(:new).and_return(http_client)
+      expect(http_client).to receive(:fetch_json).with(url).and_return(response.to_json)
+      expect(helper_module.get_org_outside_collaborators).to eq([])
     end
   end
 
