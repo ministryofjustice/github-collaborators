173d20b90b03c855c8b5a01161bd46e25419b46c
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2022-12-21 17:14:54 +0000

    Update permission

--- terraform/yjaf-csppi.tf
+++ terraform/yjaf-csppi.tf
@@ -44,7 +44,7 @@ module "yjaf-csppi" {
     },
     {
       github_user  = "craig-ashford-nec"
-      permission   = "maintain"
+      permission   = "admin"
       name         = "Craig Ashford"
       email        = "craig.ashford@necsws.com"
       org          = "NEC SWS"
