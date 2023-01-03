daec99bccf3a68fa916c777fc3682131a191c911
MatthewWhiteMoJ <61458526+MatthewWhiteMoJ@users.noreply.github.com>
2023-01-03 13:53:06 +0000

    Update moj-pttp-devicesandapps-pipeline-windows10apps.tf
    remove Jim Gregory

--- terraform/moj-pttp-devicesandapps-pipeline-windows10apps.tf
+++ terraform/moj-pttp-devicesandapps-pipeline-windows10apps.tf
@@ -1,16 +1,5 @@
 module "moj-pttp-devicesandapps-pipeline-windows10apps" {
   source     = "./modules/repository-collaborators"
   repository = "MOJ.PTTP.DevicesAndApps.Pipeline.Windows10Apps"
-  collaborators = [
-    {
-      github_user  = "jimgregory-cplusc"
-      permission   = "admin"
-      name         = "Jim Gregory"
-      email        = "jim.gregory@contentandcloud.com"
-      org          = "Content and Cloud"
-      reason       = "PTTP Tech team"
-      added_by     = "matthew.white1@justice.gov.uk"
-      review_after = "2022-12-31"
-    },
-  ]
+  collaborators = []
 }
