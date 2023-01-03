e9797d147b8fe6ee668d6fcf45489cce51d59c99
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-03 14:24:22 +0000

    extends review date for oliviergaubert (#1031)

--- terraform/yjsm-hub.tf
+++ terraform/yjsm-hub.tf
@@ -10,7 +10,7 @@ module "yjsm-hub" {
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
       added_by     = "Gareth Davies <gareth.davies@digital.justice.gov.uk> on behalf of the YJB"
-      review_after = "2022-12-31"
+      review_after = "2023-12-18"
     },
     {
       github_user  = "gregi2n"
