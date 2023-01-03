bd45da8fd209cd82086c48e4de2f427416e29686
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-03 15:59:31 +0000

    Remove empty file (#1038)
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>

--- terraform/moj-pttp-devicesandapps-pipeline-windows10apps.tf
+++
@@ -1,5 +0,0 @@
-module "moj-pttp-devicesandapps-pipeline-windows10apps" {
-  source        = "./modules/repository-collaborators"
-  repository    = "MOJ.PTTP.DevicesAndApps.Pipeline.Windows10Apps"
-  collaborators = []
-}
