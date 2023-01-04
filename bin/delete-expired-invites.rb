06f5ccfc6d0741c018e8dd5f838572949c38f545
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-04 16:15:33 +0000

    enable delete-expired-invites script

--- bin/delete-expired-invites.rb
+++ bin/delete-expired-invites.rb
@@ -1,29 +1,33 @@
-# #!/usr/bin/env ruby
+#!/usr/bin/env ruby
 
 require_relative "../lib/github_collaborators"
 
-puts "Start"
+class DeleteExpiredInvites
+  include Logging
+  include HelperModule
 
-# Create a Organization object, which Contains the Org repositories, full
-# Org members, Org outside collaborators and each repository collaborators.
-organization = GithubCollaborators::Organization.new
-
-# For each repository
-organization.repositories.each do |repository|
-  # Get the repository invites
-  repository_invites = get_repository_invites(repository_name)
-
-  # Check the repository invites
-  # using a hash like this { :login => "name", :expired => "true/false", :invite_id => "number" }
-  repository_invites.each do |invite|
-    invite_login = invite[:login]
-    invite_expired = invite[:expired]
-
-    # Delete expired invites
-    if invite_expired
-      delete_expired_invite(repository_name, invite_login)
+  def delete_expired_invites
+    repositories ||= get_active_repositories
+    repositories.each do |repository|
+      # Get the repository invites
+      repository_invites = get_repository_invites(repository.name)
+      
+      # Check the repository invites
+      # using a hash like this { :login => "name", :expired => "true/false", :invite_id => "number" }
+      repository_invites.each do |invite|
+        invite_login = invite[:login]
+        invite_expired = invite[:expired]
+        invite_id = invite[:invite_id]
+  
+        # Delete expired invites
+        if invite_expired
+          delete_expired_invite(repository.name, invite_login, invite_id)
+        end
+      end
     end
   end
 end
 
+puts "Start"
+DeleteExpiredInvites.new.delete_expired_invites
 puts "Finished"
