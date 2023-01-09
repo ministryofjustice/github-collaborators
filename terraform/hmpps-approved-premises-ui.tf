1b6e2422fc65c8294c153e4e9e087712516393c7
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-01-09 11:35:28 +0000

    Add 3 further pen. testers from NCC to 'accommodation' repos (#1056)
    hmpps-approved-premises-api is the backend of 2 "community accommodation
    service" frontends:
    
    - Approved Premises (CAS1) hmpps-approved-premises-ui
    - Temporary Accommodation (CAS3) hmpps-temporary-accommodation-ui
    
    These 3 apps are being pen. tested together by NCC.

--- terraform/hmpps-approved-premises-ui.tf
+++ terraform/hmpps-approved-premises-ui.tf
@@ -11,6 +11,36 @@ module "hmpps-approved-premises-ui" {
       reason       = "Pen. tester"
       added_by     = "Ed Davey <ed.davey@digital.justice.gov.uk>"
       review_after = "2023-02-28"
+    },
+    {
+      github_user  = "rr-ncc"
+      permission   = "pull"
+      name         = "Robert Ray"
+      email        = "robert.ray@nccgroup.com"
+      org          = "NCC"
+      reason       = "Pen. tester"
+      added_by     = "Ed Davey <ed.davey@digital.justice.gov.uk>"
+      review_after = "2023-02-28"
+    },
+    {
+      github_user  = "JonnyOrrNCC"
+      permission   = "pull"
+      name         = "Jonny Orr"
+      email        = "jonny.orr@nccgroup.com"
+      org          = "NCC"
+      reason       = "Pen. tester"
+      added_by     = "Ed Davey <ed.davey@digital.justice.gov.uk>"
+      review_after = "2023-02-28"
+    },
+    {
+      github_user  = "ThomasWellsNCC"
+      permission   = "pull"
+      name         = "Thomas Wells"
+      email        = "thomas.wells@nccgroup.com"
+      org          = "NCC"
+      reason       = "Pen. tester"
+      added_by     = "Ed Davey <ed.davey@digital.justice.gov.uk>"
+      review_after = "2023-02-28"
     }
   ]
 }
