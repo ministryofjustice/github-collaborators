039cdf69c302eff5f2f18870bc9f14bd1a931a30
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-11 16:46:44 +0000

    Update aws-trusted-advisor-to-github-issues.tf

--- terraform/aws-trusted-advisor-to-github-issues.tf
+++ terraform/aws-trusted-advisor-to-github-issues.tf
@@ -13,13 +13,13 @@ module "aws-trusted-advisor-to-github-issues" {
       review_after = "2023-02-20"
     },
     {
-      github_user  = "nick"
+      github_user  = "fkjgnreiuthwoign32342r2r"
       permission   = "admin"
       name         = "nick"
       email        = "nick@home.com"
       org          = "nick Ltd"
       reason       = "Full Org member / collaborator missing from Terraform file"
-      added_by     = "NickW"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
       review_after = "2023-02-20"
     },
   ]
