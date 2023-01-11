6264af95cb00852cbde88cf85e73fe249f13c47d
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-01-11 10:57:14 +0000

    Add full Org member / collaborator to Terraform file/s for emileswarts (#1090)
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>

--- terraform/hmpps-integration-api-docs.tf
+++ terraform/hmpps-integration-api-docs.tf
@@ -32,5 +32,15 @@ module "hmpps-integration-api-docs" {
       added_by     = "opseng-bot@digital.justice.gov.uk"
       review_after = "2023-04-11"
     },
+    {
+      github_user  = "emileswarts"
+      permission   = "admin"
+      name         = "emile swarts"
+      email        = "emile@madetech.com"
+      org          = "made tech ltd"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-04-11"
+    },
   ]
 }
