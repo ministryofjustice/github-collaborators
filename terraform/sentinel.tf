6dd1ae887bf81330c39410d99244f48f15d78560
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-03-03 13:06:18 +0000

    Update sentinel.tf (#1235)

--- terraform/sentinel.tf
+++ terraform/sentinel.tf
@@ -12,5 +12,25 @@ module "sentinel" {
       added_by     = "opseng-bot@digital.justice.gov.uk"
       review_after = "2023-05-26"
     },
+    {
+      github_user  = "sim-barnes"
+      permission   = "push"
+      name         = "simon barnes"
+      email        = "simon.barnes@civica.co.uk"
+      org          = "civica"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-05-26"
+    },
+    {
+      github_user  = "miriamgo-civica"
+      permission   = "push"
+      name         = "miriam gomez-orozco"
+      email        = "miriam.gomez-orozco@civica.co.uk"
+      org          = "civica"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-05-25"
+    },
   ]
 }
