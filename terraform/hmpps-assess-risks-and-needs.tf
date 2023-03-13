333239f6b64bdb91fcaec40b6a5cf14d558a9d2d
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-03-13 13:25:40 +0000

    Add multiple collaborators from issue (#1294)
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>

--- terraform/hmpps-assess-risks-and-needs.tf
+++ terraform/hmpps-assess-risks-and-needs.tf
@@ -1,37 +1,7 @@
-module "hmpps-delius-api" {
+module "hmpps-assess-risks-and-needs" {
   source     = "./modules/repository-collaborators"
-  repository = "hmpps-delius-api"
+  repository = "hmpps-assess-risks-and-needs"
   collaborators = [
-    {
-      github_user  = "mlaskowski4"
-      permission   = "push"
-      name         = "Michal Laskowski"
-      email        = "mlaskowski@unilink.com"
-      org          = "Unilink"
-      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
-      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"
-      review_after = "2023-08-25"
-    },
-    {
-      github_user  = "peter-bcl"
-      permission   = "push"
-      name         = "Peter Wilson"
-      email        = "pwilson@unilink.com"
-      org          = "Unilink"
-      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
-      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"
-      review_after = "2023-08-25"
-    },
-    {
-      github_user  = "seanvalmonte"
-      permission   = "push"
-      name         = "Sean Valmonte"
-      email        = "svalmonte@unilink.com"
-      org          = "Unilink"
-      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
-      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"
-      review_after = "2023-08-25"
-    },
     {
       github_user  = "aprilmd"
       permission   = "push"
--- terraform/hmpps-book-a-prison-visit-ui.tf
+++ terraform/hmpps-book-a-prison-visit-ui.tf
@@ -1,37 +1,7 @@
-module "hmpps-delius-api" {
+module "hmpps-book-a-prison-visit-ui" {
   source     = "./modules/repository-collaborators"
-  repository = "hmpps-delius-api"
+  repository = "hmpps-book-a-prison-visit-ui"
   collaborators = [
-    {
-      github_user  = "mlaskowski4"
-      permission   = "push"
-      name         = "Michal Laskowski"
-      email        = "mlaskowski@unilink.com"
-      org          = "Unilink"
-      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
-      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"
-      review_after = "2023-08-25"
-    },
-    {
-      github_user  = "peter-bcl"
-      permission   = "push"
-      name         = "Peter Wilson"
-      email        = "pwilson@unilink.com"
-      org          = "Unilink"
-      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
-      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"
-      review_after = "2023-08-25"
-    },
-    {
-      github_user  = "seanvalmonte"
-      permission   = "push"
-      name         = "Sean Valmonte"
-      email        = "svalmonte@unilink.com"
-      org          = "Unilink"
-      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
-      added_by     = "Nicola Hodgkinson <nicola.hodgkinson@justice.gov.uk>"
-      review_after = "2023-08-25"
-    },
     {
       github_user  = "aprilmd"
       permission   = "push"
