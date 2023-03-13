df6b873907578da0dc970b5446b5c44d0a47fdf4
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-03-13 15:43:21 +0000

    Add multiple collaborators from issue (#1300)
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>

---
+++ terraform/hmpps-digital-developer-recruitment.tf
@@ -0,0 +1,56 @@
+module "hmpps-digital-developer-recruitment" {
+  source     = "./modules/repository-collaborators"
+  repository = "hmpps-digital-developer-recruitment"
+  collaborators = [
+    {
+      github_user  = "aprilmd"
+      permission   = "pull"
+      name         = "april dawson"
+      email        = "april.dawson@digital.justice.gov.uk"
+      org          = "Made Tech"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-06-08"
+    },
+    {
+      github_user  = "chubberlisk"
+      permission   = "pull"
+      name         = "wen ting wang"
+      email        = "wen.tingwang@digital.justice.gov.uk"
+      org          = "Made Tech"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-06-08"
+    },
+    {
+      github_user  = "mcoffey-mt"
+      permission   = "pull"
+      name         = "matthew coffey"
+      email        = "matthew.coffey@digital.justice.gov.uk"
+      org          = "Made Tech"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-06-08"
+    },
+    {
+      github_user  = "emileswarts"
+      permission   = "pull"
+      name         = "emile swarts"
+      email        = "emile@madetech.com"
+      org          = "Made Tech"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-06-08"
+    },
+    {
+      github_user  = "bjpirt"
+      permission   = "pull"
+      name         = "ben pirt"
+      email        = "ben@madetech.com"
+      org          = "Made Tech"
+      reason       = "Full Org member / collaborator missing from Terraform file"
+      added_by     = "opseng-bot@digital.justice.gov.uk"
+      review_after = "2023-06-08"
+    },
+  ]
+}
