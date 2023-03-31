9420f8dbb9272c6eb00a528957fb5ab9f0dce8ff
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-03-31 12:39:01 +0100

    Remove expired collaborator from Terraform file/s for jazjax (#1365)
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Nick Walters <nick.walters@digital.justice.gov.uk>

--- terraform/staff-device-management-ios-apps.tf
+++ terraform/staff-device-management-ios-apps.tf
@@ -2,15 +2,5 @@ module "staff-device-management-ios-apps" {
   source     = "./modules/repository-collaborators"
   repository = "staff-device-management-ios-apps"
   collaborators = [
-    {
-      github_user  = "jazjax"
-      permission   = "admin"
-      name         = "Jasper Jackson"
-      email        = "jasper.jackson@madetech.com"
-      org          = "MadeTech"
-      reason       = "VICTOR product development"
-      added_by     = "matthew.white1@justice.gov.uk"
-      review_after = "2023-03-30"
-    },
   ]
 }
--- terraform/staff-device-management-windows10-mass-storage-controls.tf
+++ terraform/staff-device-management-windows10-mass-storage-controls.tf
@@ -2,15 +2,5 @@ module "staff-device-management-windows10-mass-storage-controls" {
   source     = "./modules/repository-collaborators"
   repository = "staff-device-management-windows10-mass-storage-controls"
   collaborators = [
-    {
-      github_user  = "jazjax"
-      permission   = "admin"
-      name         = "Jasper Jackson"
-      email        = "jasper.jackson@madetech.com"
-      org          = "MadeTech"
-      reason       = "VICTOR product development"
-      added_by     = "matthew.white1@justice.gov.uk"
-      review_after = "2023-03-30"
-    },
   ]
 }
