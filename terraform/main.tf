c058d9d9c18e2629063540660be6698d9ccc98f9
Ben Ashton <ben.ashton@digital.justice.gov.uk>
2023-04-04 13:46:42 +0100

    Test GitHub Apps authentication

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
 provider "github" {
   owner = "ministryofjustice"
-  token = var.github_token
+  # Enable this to run locally and comment out the app_auth block
+  # token = var.github_token
   #   write_delay_ms = "1000"
   #   read_delay_ms  = "1000"
+
+  app_auth {
+    id = GITHUB_APP_ID
+    installation_id = GITHUB_APP_INSTALLATION_ID
+    pem_file = GITHUB_APP_PEM_FILE
+  }
 }
