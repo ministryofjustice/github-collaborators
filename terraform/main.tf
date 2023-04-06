55f514d393534d35a358ed781f79fb99b7012ba1
Ben Ashton <61050986+ben-al@users.noreply.github.com>
2023-04-06 11:49:52 +0100

    Change GitHub Authentication to use GitHub Apps instead of PAT (#1376)
    * Test GitHub Apps authentication
    
    * Commit changes made by code formatters
    
    * Test 2
    
    * Test3
    
    * Commit changes made by code formatters
    
    * test5
    
    * Fall back to environment variables for secrets
    
    * Commit changes made by code formatters
    
    * Remove space
    
    * Fix secret names
    
    * Remove test changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
 provider "github" {
   owner = "ministryofjustice"
-  token = var.github_token
+  # Enable this to run locally and comment out the app_auth block
+  # token = var.github_token
   #   write_delay_ms = "1000"
   #   read_delay_ms  = "1000"
+
+  # This uses environment variables stored in GitHub secrets to authenticate, comment this out to run locally and use token auth
+  # https://registry.terraform.io/providers/integrations/github/latest/docs
+  app_auth {}
 }
