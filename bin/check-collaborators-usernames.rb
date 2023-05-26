e2d483c4df432fd74108e184bc9a0a69e92f1a2b
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-05-26 15:58:04 +0100

    Use existing Ruby to check collaborator username is valid in PR (#1495)
    Co-authored-by: Operations Engineering Bot <github-actions[bot]@users.noreply.github.com>

---
+++ bin/check-collaborators-usernames.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require_relative "../lib/github_collaborators"
+
+class CheckCollaboratorUsernames
+  include Logging
+  include HelperModule
+
+  def start
+    all_collaborators = []
+    http_client = GithubCollaborators::HttpClient.new
+    the_terraform_files = GithubCollaborators::TerraformFiles.new
+    the_terraform_files.get_terraform_files.each do |terraform_file|
+      all_collaborators += the_terraform_files.get_collaborators_in_file(terraform_file.filename)
+    end
+
+    all_collaborators.sort!
+    all_collaborators.uniq!
+
+    all_collaborators.each do |collaborator|
+      url = "#{GH_URL}/users/#{collaborator}"
+      http_code = http_client.fetch_code(url)
+      if http_code != "200"
+        logger.error("Collaborator #{collaborator} username not found on GitHub")
+        exit(1)
+      end
+    end
+  end
+end
+
+puts "Start"
+CheckCollaboratorUsernames.new.start
+puts "Finished"
