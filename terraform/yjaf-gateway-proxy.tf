48096f465e321c5407a3c435698fc80584dff72d
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-09-01 13:41:11 +0100

    Add collaborator

--- terraform/yjaf-gateway-proxy.tf
+++ terraform/yjaf-gateway-proxy.tf
@@ -252,5 +252,15 @@ module "yjaf-gateway-proxy" {
       added_by     = "mick.ewers@yjb.gov.uk"
       review_after = "2024-07-17"
     },
+    {
+      github_user  = "ttipler"
+      permission   = "admin"
+      name         = "Thomas Tipler"
+      email        = "thomas.tipler@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "Devops guys need access to make app/infra changes"
+      added_by     = "mick.ewers@yjb.gov.uk"
+      review_after = "2024-09-01"
+    },
   ]
 }
