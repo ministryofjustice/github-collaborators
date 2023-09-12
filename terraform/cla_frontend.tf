8924a5dd11b27ee1ff131a3c4698084cb2881d50
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-09-12 13:04:21 +0100

    Update collaborator permission

--- terraform/cla_frontend.tf
+++ terraform/cla_frontend.tf
@@ -4,7 +4,7 @@ module "cla_frontend" {
   collaborators = [
     {
       github_user  = "BenMillar-CGI"
-      permission   = "push"
+      permission   = "admin"
       name         = "Ben Millar"
       email        = "ben.millar@digital.justice.gov.uk"
       org          = "CGI"
