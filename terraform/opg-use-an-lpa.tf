00df791cdb4976291affd05b358ff765fd4b4659
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-09-15 11:22:39 +0100

    Add collaborator

---
+++ terraform/opg-use-an-lpa.tf
@@ -0,0 +1,16 @@
+module "opg-use-an-lpa" {
+  source     = "./modules/repository-collaborators"
+  repository = "opg-use-an-lpa"
+  collaborators = [
+    {
+      github_user  = "weblate"
+      permission   = "push"
+      name         = "Weblate Push User"
+      email        = "hosted@weblate.org"
+      org          = "Ministry of Justice"
+      reason       = "Used by Weblate"
+      added_by     = "adam.cooper@digital.justice.gov.uk"
+      review_after = "2024-10-10"
+    },
+  ]
+}
