49caeb2c17e974d1f81911b37322efa7709690d4
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-10-10 11:51:06 +0100

    Remove expired collaborators (#1839)

--- terraform/cica-apply-prototype.tf
+++ terraform/cica-apply-prototype.tf
@@ -3,26 +3,6 @@ module "cica-apply-prototype" {
   repository = "cica-apply-prototype"
   collaborators = [
     {
-      github_user  = "bsi0714"
-      permission   = "push"
-      name         = "daniel elliott"
-      email        = "daniel.elliott@bsigroup.com"
-      org          = "bsi"
-      reason       = "Full Org member / collaborator missing from Terraform file"
-      added_by     = "opseng-bot@digital.justice.gov.uk"
-      review_after = "2023-12-20"
-    },
-    {
-      github_user  = "jp-bsi"
-      permission   = "push"
-      name         = "joe phee"
-      email        = "joseph.phee@bsigroup.com"
-      org          = "bsi"
-      reason       = "Full Org member / collaborator missing from Terraform file"
-      added_by     = "opseng-bot@digital.justice.gov.uk"
-      review_after = "2023-12-20"
-    },
-    {
       github_user  = "msenkiw"
       permission   = "push"
       name         = "michael senkiw"
