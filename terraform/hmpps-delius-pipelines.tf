d19db133aa25a6fe8517dc414fb52a72e2b4dd70
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-11-01 15:35:16 +0000

    Remove Full Org Members from Collaborator Modules

--- terraform/hmpps-delius-pipelines.tf
+++ terraform/hmpps-delius-pipelines.tf
@@ -3,36 +3,6 @@ module "hmpps-delius-pipelines" {
   repository = "hmpps-delius-pipelines"
   collaborators = [
     {
-      github_user  = "simoncreasy-civica"
-      permission   = "push"
-      name         = "Simon Creasy"
-      email        = "simon.creasy@civica.co.uk"
-      org          = "civica"
-      reason       = "HMPPS related work"
-      added_by     = "vincent.cheung@digital.justice.gov.uk"
-      review_after = "2023-12-20"
-    },
-    {
-      github_user  = "sim-barnes"
-      permission   = "push"
-      name         = "Simon Barnes"
-      email        = "simon.barnes@civica.co.uk"
-      org          = "civica"
-      reason       = "HMPPS related work"
-      added_by     = "vincent.cheung@digital.justice.gov.uk"
-      review_after = "2023-12-20"
-    },
-    {
-      github_user  = "miriamgo-civica"
-      permission   = "push"
-      name         = "Miriam Gomez-Orozco"
-      email        = "Miriam.Gomez-Orozco@civica.co.uk"
-      org          = "civica"
-      reason       = "HMPPS related work"
-      added_by     = "vincent.cheung@digital.justice.gov.uk"
-      review_after = "2023-12-20"
-    },
-    {
       github_user  = "peter-bcl"
       permission   = "push"
       name         = "Peter Wilson"
