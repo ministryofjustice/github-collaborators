60a121cdbf0568e18be1777d543f842ae375e251
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-12-01 15:53:12 +0000

    Extend review dates

--- terraform/bichard7-next-ui.tf
+++ terraform/bichard7-next-ui.tf
@@ -10,7 +10,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "monotypical"
@@ -20,7 +20,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "joe-fol"
@@ -30,7 +30,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "BushraAbdullahi"
@@ -40,7 +40,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "ian-antking"
@@ -50,7 +50,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "richardcane"
@@ -60,7 +60,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "senny10"
@@ -70,7 +70,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "richrace"
@@ -80,7 +80,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "seth-barber"
@@ -90,7 +90,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
     {
       github_user  = "ammaar-limbada-madetech"
@@ -100,7 +100,7 @@ module "bichard7-next-ui" {
       org          = "Madetech"
       reason       = "CJSE Bichard Development"
       added_by     = "dom.tomkins@digital.justice.gov.uk"
-      review_after = "2023-12-31"
+      review_after = "2024-12-31"
     },
   ]
 }
