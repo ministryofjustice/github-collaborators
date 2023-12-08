9019ee330fdc5f3d4bc6d2044e3c76217d68f976
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-12-08 11:39:40 +0000

    Remove full org member related code

--- spec/terraform_block_spec.rb
+++ spec/terraform_block_spec.rb
@@ -6,7 +6,7 @@ class GithubCollaborators
     context "test TerraformBlock" do
       before do
         @terraform_block = GithubCollaborators::TerraformBlock.new
-        @review_date = (Date.today + 90).strftime(DATE_FORMAT)
+        @review_date = Date.today.strftime(DATE_FORMAT)
       end
 
       def check_terraform_block_empty(terraform_block)
@@ -30,8 +30,7 @@ class GithubCollaborators
       end
 
       it "call add_terraform_file_collaborator_data" do
-        review_date = Date.today.strftime(DATE_FORMAT)
-        collaborator_data = create_collaborator_data(review_date)
+        collaborator_data = create_collaborator_data(@review_date)
         @terraform_block.add_terraform_file_collaborator_data(collaborator_data)
         test_equal(@terraform_block.username, TEST_COLLABORATOR_LOGIN)
         test_equal(@terraform_block.permission, TEST_COLLABORATOR_PERMISSION)
@@ -40,7 +39,7 @@ class GithubCollaborators
         test_equal(@terraform_block.org, TEST_COLLABORATOR_ORG)
         test_equal(@terraform_block.reason, TEST_COLLABORATOR_REASON)
         test_equal(@terraform_block.added_by, TEST_COLLABORATOR_ADDED_BY)
-        test_equal(@terraform_block.review_after, review_date)
+        test_equal(@terraform_block.review_after, @review_date)
       end
 
       it "call add_collaborator_email_address" do
@@ -67,47 +66,18 @@ class GithubCollaborators
         test_equal(@terraform_block.review_after, "")
       end
 
-      it "call add_org_member_collaborator_data" do
-        collaborator = GithubCollaborators::FullOrgMember.new(TEST_COLLABORATOR_LOGIN)
-        collaborator.add_info_from_file(TEST_COLLABORATOR_EMAIL, TEST_COLLABORATOR_NAME, TEST_COLLABORATOR_ORG)
-        @terraform_block.add_org_member_collaborator_data(collaborator, TEST_COLLABORATOR_PERMISSION)
-        test_equal(@terraform_block.username, TEST_COLLABORATOR_LOGIN)
-        test_equal(@terraform_block.permission, TEST_COLLABORATOR_PERMISSION)
-        test_equal(@terraform_block.name, TEST_COLLABORATOR_NAME)
-        test_equal(@terraform_block.email, TEST_COLLABORATOR_EMAIL)
-        test_equal(@terraform_block.org, TEST_COLLABORATOR_ORG)
-        test_equal(@terraform_block.reason, REASON1)
-        test_equal(@terraform_block.added_by, ADDED_BY_EMAIL)
-        test_equal(@terraform_block.review_after, @review_date)
-      end
-
-      it "call add_missing_collaborator_data" do
-        @terraform_block.add_missing_collaborator_data(TEST_COLLABORATOR_LOGIN)
-        test_equal(@terraform_block.username, TEST_COLLABORATOR_LOGIN)
-        test_equal(@terraform_block.permission, "")
-        test_equal(@terraform_block.name, "")
-        test_equal(@terraform_block.email, "")
-        test_equal(@terraform_block.org, "")
-        test_equal(@terraform_block.reason, REASON2)
-        test_equal(@terraform_block.added_by, ADDED_BY_EMAIL)
-        test_equal(@terraform_block.review_after, @review_date)
-      end
-
       it "call revert_block" do
-        @terraform_block.add_terraform_file_collaborator_data({})
-        check_terraform_block_empty(@terraform_block)
-        terraform_block2 = GithubCollaborators::TerraformBlock.new
-        terraform_block2.add_missing_collaborator_data(TEST_COLLABORATOR_LOGIN)
-        test_equal(terraform_block2.username, TEST_COLLABORATOR_LOGIN)
-        test_equal(terraform_block2.permission, "")
-        test_equal(terraform_block2.name, "")
-        test_equal(terraform_block2.email, "")
-        test_equal(terraform_block2.org, "")
-        test_equal(terraform_block2.reason, REASON2)
-        test_equal(terraform_block2.added_by, ADDED_BY_EMAIL)
-        test_equal(terraform_block2.review_after, @review_date)
-        terraform_block2.revert_block(@terraform_block)
-        check_terraform_block_empty(terraform_block2)
+        terraform_block = create_test_data(@review_date)
+        test_equal(terraform_block.username, TEST_COLLABORATOR_LOGIN)
+        test_equal(terraform_block.permission, TEST_COLLABORATOR_PERMISSION)
+        test_equal(terraform_block.name, TEST_COLLABORATOR_NAME)
+        test_equal(terraform_block.email, TEST_COLLABORATOR_EMAIL)
+        test_equal(terraform_block.org, TEST_COLLABORATOR_ORG)
+        test_equal(terraform_block.reason, TEST_COLLABORATOR_REASON)
+        test_equal(terraform_block.added_by, TEST_COLLABORATOR_ADDED_BY)
+        test_equal(terraform_block.review_after, @review_date)
+        terraform_block.revert_block(terraform_block)
+        check_terraform_block_empty(terraform_block)
       end
     end
   end
