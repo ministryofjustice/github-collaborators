a26d172c60e7a89ca7dbdeef31892d139e8ded00
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-02-16 11:07:11 +0000

    Add collaborator

--- terraform/yjaf-utilities.tf
+++ terraform/yjaf-utilities.tf
@@ -52,5 +52,15 @@ module "yjaf-utilities" {
       added_by     = "mick.ewers@yjb.gov.uk"
       review_after = "2024-06-30"
     },
+    {
+      github_user  = "joharveynec"
+      permission   = "pull"
+      name         = "Jo Harvey"
+      email        = "jo.harvey@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "To be able to view and manage vulnerabilities in YJAF code via Snyk"
+      added_by     = "mick.ewers@yjb.gov.uk"
+      review_after = "2025-02-15"
+    },
   ]
 }
