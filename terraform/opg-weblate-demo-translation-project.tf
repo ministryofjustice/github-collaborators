ba9103ebbe2c4ebc5c21687b9f3d39af6b83955a
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-02-21 15:40:35 +0000

    Adds Weblate bot to demo translation project

---
+++ terraform/opg-weblate-demo-translation-project.tf
@@ -0,0 +1,16 @@
+module "opg-weblate-demo-translation-project" {
+  source     = "./modules/repository-collaborators"
+  repository = "opg-weblate-demo-translation-project"
+  collaborators = [
+    {
+      github_user  = "weblate"
+      permission   = "push"
+      name         = "Weblate Push User"
+      email        = "hosted@weblate.org"
+      org          = "Ministry of Justice"
+      reason       = "Used by Weblate"
+      added_by     = "andrew.pearce@digital.justice.gov.uk"
+      review_after = "2024-10-10"
+    },
+  ]
+}
