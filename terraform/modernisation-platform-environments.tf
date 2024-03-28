f62bc2c2470c14f26549c472313cdaaa51131629
Silviana Horga <silviana.horga@digital.justice.gov.uk>
2024-03-28 14:59:09 +0000

    Remove access to collaborators who are rolling off from cjse-xhibit-portal project

--- terraform/modernisation-platform-environments.tf
+++ terraform/modernisation-platform-environments.tf
@@ -123,16 +123,6 @@ module "modernisation-platform-environments" {
       review_after = "2024-07-28"
     },
     {
-      github_user  = "christok-moj"
-      permission   = "push"
-      name         = "Christopher Kai"
-      email        = "christopher.kai@version1.com"
-      org          = "Version 1"
-      reason       = "Get access to xhibit-portal on Modernisation Platform"
-      added_by     = "silviana.horga@digital.justice.gov.uk"
-      review_after = "2024-08-01"
-    },
-    {
       github_user  = "kevinb-v1-uk"
       permission   = "push"
       name         = "Kevin Brandon"
--- terraform/moj-cjse-xhibit-portal-v2.tf
+++ terraform/moj-cjse-xhibit-portal-v2.tf
@@ -13,16 +13,6 @@ module "moj-cjse-xhibit-portal-v2" {
       review_after = "2024-09-25"
     },
     {
-      github_user  = "BertONeill-V1"
-      permission   = "push"
-      name         = "Bert O'Neill"
-      email        = "bert.oneill@version1.com"
-      org          = "Version 1"
-      reason       = "Needs to be able to view, submit and approve pull requests, push access to xhibit-portal repositories"
-      added_by     = "silviana.horga@digital.justice.gov.uk"
-      review_after = "2024-09-25"
-    },
-    {
       github_user  = "raydevlinv1"
       permission   = "push"
       name         = "Ray Devlin"
