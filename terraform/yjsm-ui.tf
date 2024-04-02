b49d5c0e9cdda236794307cfdeb913c3a7f96031
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-04-02 13:34:45 +0100

    Extend review date

--- terraform/yjsm-ui.tf
+++ terraform/yjsm-ui.tf
@@ -19,8 +19,8 @@ module "yjsm-ui" {
       email        = "javaid.arshad@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "New YJAF Full Stack Developer"
-      added_by     = "mick.ewers@yjb.gov.uk"
-      review_after = "2024-05-02"
+      added_by     = "David.Hall@yjb.gov.uk"
+      review_after = "2024-12-23"
     },
     {
       github_user  = "oliviergaubert"
