ecacc774173d1cb3a9c6d3de7429a7a622a2e483
Silviana Horga <silviana.horga@digital.justice.gov.uk>
2024-04-04 15:38:05 +0100

    Grant developer access to cjse-xhibit-portal repos

--- terraform/moj-cjse-xhibit-portal.tf
+++ terraform/moj-cjse-xhibit-portal.tf
@@ -42,5 +42,15 @@ module "moj-cjse-xhibit-portal" {
       added_by     = "silviana.horga@digital.justice.gov.uk"
       review_after = "2024-09-25"
     },
+    {
+      github_user  = "jbantrotu"
+      permission   = "push"
+      name         = "Jagadeesh Bantrotu"
+      email        = "jagadeesh.bantrotu@version1.com"
+      org          = "Version 1"
+      reason       = "Needs to be able to view, submit and approve pull requests, push access to xhibit-portal repositories"
+      added_by     = "silviana.horga@digital.justice.gov.uk"
+      review_after = "2024-09-25"
+    },
   ]
 }
