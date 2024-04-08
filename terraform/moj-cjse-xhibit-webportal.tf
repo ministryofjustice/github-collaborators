4869f62a97ce88834a8e5334dac51c06fcd1b81c
Silviana Horga <silviana.horga@digital.justice.gov.uk>
2024-04-08 16:16:58 +0100

    Add developers as collaborators to cjse-xhibit webportal repos

---
+++ terraform/moj-cjse-xhibit-webportal.tf
@@ -0,0 +1,46 @@
+module "moj-cjse-xhibit-webportal" {
+  source     = "./modules/repository-collaborators"
+  repository = "moj-cjse-xhibit-webportal"
+  collaborators = [
+    {
+      github_user  = "kevinb-v1-uk"
+      permission   = "admin"
+      name         = "Kevin Brandon"
+      email        = "kevin.brandon@version1.com"
+      org          = "Version 1"
+      reason       = "Needs admin rights to xhibit-webportal repository"
+      added_by     = "silviana.horga@digital.justice.gov.uk"
+      review_after = "2024-09-25"
+    },
+    {
+      github_user  = "jbantrotu"
+      permission   = "admin"
+      name         = "Jagadeesh Bantrotu"
+      email        = "jagadeesh.bantrotu@version1.com"
+      org          = "Version 1"
+      reason       = "Needs admin rights to xhibit-webportal repository"
+      added_by     = "silviana.horga@digital.justice.gov.uk"
+      review_after = "2024-09-25"
+    },
+    {
+      github_user  = "raydevlinv1"
+      permission   = "admin"
+      name         = "Ray Devlin"
+      email        = "ray.devlin@version1.com"
+      org          = "Version 1"
+      reason       = "Needs admin rights to xhibit-webportal repository"
+      added_by     = "silviana.horga@digital.justice.gov.uk"
+      review_after = "2024-09-25"
+    },
+    {
+      github_user  = "PeteWV1"
+      permission   = "admin"
+      name         = "Peter Wightman"
+      email        = "peter.wightman@version1.com"
+      org          = "Version 1"
+      reason       = "Needs admin rights to xhibit-webportal repository"
+      added_by     = "silviana.horga@digital.justice.gov.uk"
+      review_after = "2024-09-25"
+    },
+  ]
+}
