ddb05153f1d4f7d7649f080ea8c24f77fbbc22ce
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-04-08 11:10:32 +0100

    Add new collaborator and update MoJ approver

--- terraform/yjaf-gateway-proxy.tf
+++ terraform/yjaf-gateway-proxy.tf
@@ -19,7 +19,7 @@ module "yjaf-gateway-proxy" {
       email        = "stephen.hobden@necsws.com"
       org          = "NECSWS"
       reason       = "Developer Requiring access to work on gateway-proxy project"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-05-02"
     },
     {
@@ -29,7 +29,7 @@ module "yjaf-gateway-proxy" {
       email        = "philip.gilgunn@necsws.com"
       org          = "NECSWS"
       reason       = "Head of Technology"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-05-02"
     },
     {
@@ -39,7 +39,7 @@ module "yjaf-gateway-proxy" {
       email        = "fabio.salvarezza@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-06-30"
     },
     {
@@ -49,7 +49,7 @@ module "yjaf-gateway-proxy" {
       email        = "jo.harvey@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "To be able to view and manage vulnerabilities in YJAF code via Snyk"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-02-15"
     },
     {
@@ -59,8 +59,18 @@ module "yjaf-gateway-proxy" {
       email        = "vasil.dimitrov@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-02-16"
+    },
+    {
+      github_user  = "andrewtrichards"
+      permission   = "push"
+      name         = "Andrew Richards"
+      email        = "andrew.richards1@necsws.com"
+      org          = "NEC SWS"
+      reason       = "To be able to continue development of the project as architect/developer"
+      added_by     = "David.Hall@yjb.gov.uk"
+      review_after = "2025-04-04"
     },
   ]
 }
