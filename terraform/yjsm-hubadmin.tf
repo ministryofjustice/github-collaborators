8cc3ee6d5a4f301519dd1d0da435d043c1a2e8ec
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-04-18 15:03:20 +0100

    Change collaborator permission

--- terraform/yjsm-hubadmin.tf
+++ terraform/yjsm-hubadmin.tf
@@ -9,7 +9,7 @@ module "yjsm-hubadmin" {
       email        = "olivier.gaubert@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-06-15"
     },
     {
@@ -19,7 +19,7 @@ module "yjsm-hubadmin" {
       email        = "thomas.tipler@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Devops guys need access to make app/infra changes"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-07-15"
     },
     {
@@ -34,12 +34,12 @@ module "yjsm-hubadmin" {
     },
     {
       github_user  = "andrewtrichards"
-      permission   = "admin"
+      permission   = "push"
       name         = "Andrew Richards"
       email        = "andrew.richards1@necsws.com"
       org          = "NEC SWS"
       reason       = "Existing developer"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-06-01"
     },
     {
@@ -49,7 +49,7 @@ module "yjsm-hubadmin" {
       email        = "angela.site@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "YJAF (new developer)"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-07-17"
     },
     {
@@ -59,7 +59,7 @@ module "yjsm-hubadmin" {
       email        = "stephen.hobden@necsws.com"
       org          = "NECSWS"
       reason       = "Want to amend logging settings for all microservices"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-07-09"
     },
     {
@@ -69,7 +69,7 @@ module "yjsm-hubadmin" {
       email        = "fabio.salvarezza@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2024-06-30"
     },
     {
@@ -79,7 +79,7 @@ module "yjsm-hubadmin" {
       email        = "jo.harvey@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "To be able to view and manage vulnerabilities in YJAF code via Snyk"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-02-15"
     },
     {
@@ -89,7 +89,7 @@ module "yjsm-hubadmin" {
       email        = "vasil.dimitrov@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
-      added_by     = "mick.ewers@yjb.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-02-16"
     },
   ]
