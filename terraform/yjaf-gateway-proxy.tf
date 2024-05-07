cbc2c596b7281c51cd30cabbb50d0d033e83899e
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-05-07 11:56:05 +0100

    Add collaborator

--- terraform/yjaf-gateway-proxy.tf
+++ terraform/yjaf-gateway-proxy.tf
@@ -82,5 +82,15 @@ module "yjaf-gateway-proxy" {
       added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-04-22"
     },
+    {
+      github_user  = "gregi2n"
+      permission   = "admin"
+      name         = "Greg Whiting"
+      email        = "greg.whiting@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "Part of the NEC supplier team for the YJB YJAF system"
+      added_by     = "David.Hall@yjb.gov.uk"
+      review_after = "2024-12-23"
+    },
   ]
 }
