d44ead67496f2b68c5617cb7d55102a95b5154bb
federicostaianojustice1 <86769850+federicostaianojustice1@users.noreply.github.com>
2024-05-20 10:59:41 +0100

    Update pfs-software-services-pipelines.tf
    Adding Kainos Collaborators modernization project

--- terraform/pfs-software-services-pipelines.tf
+++ terraform/pfs-software-services-pipelines.tf
@@ -32,5 +32,15 @@ module "pfs-software-services-pipelines" {
       added_by     = "federico.staiano1@justice.gov.uk"
       review_after = "2024-07-30"
     },
+    {
+      github_user  = "MisterSK"
+      permission   = "admin"
+      name         = "Sanya Khasenye"
+      email        = "s.khasenye@kainos.com"
+      org          = "Kainos"
+      reason       = "Kainos is working on new modernization platform for Unilink services"
+      added_by     = "federico.staiano1@justice.gov.uk"
+      review_after = "2024-07-30"
+    },
   ]
 }
