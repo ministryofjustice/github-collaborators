c1815c15b02d912acc3fa933b16516b3e806539a
Marcus Aspin <maspin@unilink.com>
2024-05-23 16:12:27 +0100

    Add access for DavidJohnReid

--- terraform/delius-manual-deployments.tf
+++ terraform/delius-manual-deployments.tf
@@ -12,5 +12,15 @@ module "delius-manual-deployments" {
       added_by     = "marcus.aspin@digital.justice.gov.uk"
       review_after = "2025-01-12"
     },
+    {
+      github_user  = "DavidJohnReid"
+      permission   = "admin"
+      name         = "David Reid"
+      email        = "dreid@unilink.com"
+      org          = "Unilink"
+      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
+      added_by     = "marcus.aspin@digital.justice.gov.uk"
+      review_after = "2025-01-12"
+    }
   ]
 }
