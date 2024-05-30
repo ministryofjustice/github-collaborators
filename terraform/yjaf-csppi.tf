4f1cfc2eca7c134b95ac774b4c089832b570f621
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-05-30 16:08:52 +0100

    Additional repositories added

---
+++ terraform/yjaf-csppi.tf
@@ -0,0 +1,16 @@
+module "yjaf-csppi" {
+  source     = "./modules/repository-collaborators"
+  repository = "yjaf-csppi"
+  collaborators = [
+    {
+      github_user  = "codebysachin"
+      permission   = "push"
+      name         = "Sachin Gupta"
+      email        = "sachin.gupta@necsws.com"
+      org          = "NEC Software Solutions"
+      reason       = "Part of the NEC supplier team for the YJB YJAF system"
+      added_by     = "sam.pepper@digital.justice.gov.uk"
+      review_after = "2025-05-29"
+    },
+  ]
+}
