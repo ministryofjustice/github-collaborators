df1b9803ac84272ba63f02ff99445e8a1354ffea
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-05-31 11:25:14 +0100

    Switched to use correct approval email

--- terraform/yjaf-returns.tf
+++ terraform/yjaf-returns.tf
@@ -119,7 +119,7 @@ module "yjaf-returns" {
       email        = "sachin.gupta@necsws.com"
       org          = "NEC Software Solutions"
       reason       = "Part of the NEC supplier team for the YJB YJAF system"
-      added_by     = "sam.pepper@digital.justice.gov.uk"
+      added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-05-29"
     },
   ]
