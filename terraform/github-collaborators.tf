0fd927ca44ec33580975e3bb60cc68ae22daf2de
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-06-27 13:21:55 +0100

    Add collaborator

---
+++ terraform/github-collaborators.tf
@@ -0,0 +1,16 @@
+module "github-collaborators" {
+  source     = "./modules/repository-collaborators"
+  repository = "github-collaborators"
+  collaborators = [
+    {
+      github_user  = "richardcane"
+      permission   = "push"
+      name         = "Richard Cane"
+      email        = "richard.cane@madetech.com"
+      org          = "Madetech"
+      reason       = "To allow collaborator to raise PRs to manage off-boarding of other collaborators"
+      added_by     = "antony.bishop@digital.justice.gov.uk"
+      review_after = "2024-12-27"
+    },
+  ]
+}
