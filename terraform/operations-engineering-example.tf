659488ef2e11dbf2cbf7a321d89c0bcd2f307424
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-07-02 17:37:42 +0100

    Test add collaborator

---
+++ terraform/operations-engineering-example.tf
@@ -0,0 +1,16 @@
+module "opg-use-an-lpa" {
+  source     = "./modules/repository-collaborators"
+  repository = "operations-engineering-example"
+  collaborators = [
+    {
+      github_user  = "crushaforbes"
+      permission   = "push"
+      name         = "Crusha Forbes"
+      email        = "crushaforbes@gmail.com"
+      org          = "Testing"
+      reason       = "Test new token works"
+      added_by     = "tamsin.forbes@digital.justice.gov.uk"
+      review_after = "2024-07-09"
+    },
+  ]
+}
