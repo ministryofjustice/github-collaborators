94e36c71dbad95a4a1a3a342a5b5b6e79d389ec0
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-08-05 09:40:28 +0100

    Remove collaborators

--- terraform/cjse-test.tf
+++ terraform/cjse-test.tf
@@ -3,26 +3,6 @@ module "cjse-test" {
   repository = "cjse-test"
   collaborators = [
     {
-      github_user  = "jbantrotu"
-      permission   = "admin"
-      name         = "Jagadeesh Bantrotu"
-      email        = "jagadeesh.bantrotu@version1.com"
-      org          = "Version 1"
-      reason       = "Get access to xhibit-portal on Cloud Platform"
-      added_by     = "shahzad.chaudhry@digital.justice.gov.uk"
-      review_after = "2024-08-01"
-    },
-    {
-      github_user  = "raydevlinv1"
-      permission   = "admin"
-      name         = "Ray Devlin"
-      email        = "ray.devlin@version1.com"
-      org          = "Version 1"
-      reason       = "Get access to xhibit-portal on Cloud Platform"
-      added_by     = "shahzad.chaudhry@digital.justice.gov.uk"
-      review_after = "2024-08-01"
-    },
-    {
       github_user  = "zimboflyman"
       permission   = "admin"
       name         = "Shane Vanleeuwen"
@@ -41,26 +21,6 @@ module "cjse-test" {
       reason       = "Get access to xhibit-portal on Cloud Platform"
       added_by     = "silviana.horga@digital.justice.gov.uk"
       review_after = "2024-09-25"
-    },
-    {
-      github_user  = "Klaw29"
-      permission   = "admin"
-      name         = "Kay Babatunde"
-      email        = "kbabatunde@planittesting.com"
-      org          = "Version 1"
-      reason       = "Get access to xhibit-portal on Cloud Platform"
-      added_by     = "shahzad.chaudhry@digital.justice.gov.uk"
-      review_after = "2024-08-01"
-    },
-    {
-      github_user  = "mdestani"
-      permission   = "admin"
-      name         = "Mond Destani"
-      email        = "mdestani@planittesting.com"
-      org          = "Version 1"
-      reason       = "Get access to xhibit-portal on Cloud Platform"
-      added_by     = "shahzad.chaudhry@digital.justice.gov.uk"
-      review_after = "2024-08-01"
     },
   ]
 }
