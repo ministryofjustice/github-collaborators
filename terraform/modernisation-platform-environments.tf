4a0738f214c6faf72840dd693016285793d42fd9
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-08-08 11:55:17 +0100

    Add new collaborator

--- terraform/modernisation-platform-environments.tf
+++ terraform/modernisation-platform-environments.tf
@@ -42,5 +42,15 @@ module "modernisation-platform-environments" {
       added_by     = "edward.proctor@digital.justice.gov.uk"
       review_after = "2024-12-14"
     },
+    {
+      github_user  = "djcostin"
+      permission   = "push"
+      name         = "Daniel Costin"
+      email        = "daniel.costin@digital.justice.gov.uk"
+      org          = "Modular Data"
+      reason       = "Onboarding of new full stack developer, needs access for day to day work."
+      added_by     = "mark.mckeown@digital.justice.gov.uk"
+      review_after = "2025-03-31"
+    },
   ]
 }
