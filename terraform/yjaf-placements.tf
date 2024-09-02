58118eeabaf4dff3874cae37eb581ada3f876ee4
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-09-02 14:04:26 +0100

    Readd Andrew Richards as an outside collaborator

--- terraform/yjaf-placements.tf
+++ terraform/yjaf-placements.tf
@@ -102,5 +102,15 @@ module "yjaf-placements" {
       added_by     = "David.Hall@yjb.gov.uk"
       review_after = "2025-05-29"
     },
+    {
+      github_user  = "andrewtrichards"
+      permission   = "admin"
+      name         = "Andrew Richards"
+      email        = "andrew.richards1@necsws.com"
+      org          = "NEC SWS"
+      reason       = "Existing developer"
+      added_by     = "David.Hall@yjb.gov.uk"
+      review_after = "2025-03-01"
+    },
   ]
 }
