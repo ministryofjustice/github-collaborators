364d141cd2cba7d98f54b923b642e3e1b98fb4c4
federicostaianojustice1 <86769850+federicostaianojustice1@users.noreply.github.com>
2024-09-02 14:44:38 +0100

    Create pfs-software-services_ansible.tf
    Kainos Collaborators

---
+++ terraform/pfs-software-services_ansible.tf
@@ -0,0 +1,26 @@
+module "pfs-software-services_ansible" {
+  source     = "./modules/repository-collaborators"
+  repository = "pfs-software-terraform-modules"
+  collaborators = [
+    {
+      github_user  = "nathanials"
+      permission   = "admin"
+      name         = "Nathanials Stewart"
+      email        = "n.stewart@kainos.com"
+      org          = "Kainos"
+      reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo for Jenkins"
+      added_by     = "jonathan.houston@justice.gov.uk"
+      review_after = "2025-01-26"
+    },
+    {
+      github_user  = "simongivan"
+      permission   = "admin"
+      name         = "Simon Givan"
+      email        = "s.givan@kainos.com"
+      org          = "Kainos"
+      reason       = "Kainos is working on transfering code from a kainos owned repo to an MOJ owned repo"
+      added_by     = "jonathan.houston@justice.gov.uk"
+      review_after = "2025-01-26"
+    },
+  ]
+}
