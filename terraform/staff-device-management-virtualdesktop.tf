e8dd75b5cb308661c461e6f0195225917b485b4c
andyrogers1973 <andy.rogers@digital.justice.gov.uk>
2024-09-03 15:11:41 +0100

    Updates collaborator request

--- terraform/staff-device-management-virtualdesktop.tf
+++ terraform/staff-device-management-virtualdesktop.tf
@@ -3,27 +3,7 @@ module "staff-device-management-virtualdesktop" {
   repository = "staff-device-management-virtualdesktop"
   collaborators = [
     {
-      github_user  = ""
-      permission   = "push"
-      name         = "Catrina Feely"
-      email        = "cfeely@microsoft.com"
-      org          = "Microsoft"
-      reason       = "MoJO AVD build into a Microsoft Windows 365 product"
-      added_by     = "Leanne.Emans@justice.gov.uk"
-      review_after = "2024-12-01"
-    },
-    {
-      github_user  = ""
-      permission   = "push"
-      name         = "Renu Sandy"
-      email        = "resandy@microsoft.com"
-      org          = "Microsoft"
-      reason       = "MoJO AVD build into a Microsoft Windows 365 product"
-      added_by     = "Leanne.Emans@justice.gov.uk"
-      review_after = "2024-12-01"
-    },
-    {
-      github_user  = ""
+      github_user  = "NickyScout"
       permission   = "push"
       name         = "Nikolay Milyaev"
       email        = "nmilyaev@microsoft.com"
