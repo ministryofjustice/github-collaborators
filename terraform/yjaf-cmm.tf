0a85040e53fa875e602fe8588e13a44187f7b0c1
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-09-30 11:29:11 +0100

    Extend collaborator review date

--- terraform/yjaf-cmm.tf
+++ terraform/yjaf-cmm.tf
@@ -30,7 +30,7 @@ module "yjaf-cmm" {
       org          = "NEC Software Solutions"
       reason       = "New YJAF Full Stack Developer"
       added_by     = "David.Hall@yjb.gov.uk"
-      review_after = "2024-10-29"
+      review_after = "2025-03-31"
     },
     {
       github_user  = "angelanec"
