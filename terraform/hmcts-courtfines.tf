be7067b9d19c48386ae083bce664568deb2db391
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-10-10 20:45:13 +0100

    Add collaborator

--- terraform/hmcts-courtfines.tf
+++ terraform/hmcts-courtfines.tf
@@ -4,13 +4,23 @@ module "hmcts-courtfines" {
   collaborators = [
     {
       github_user  = "jrichards93-hmcts"
-      permission   = "push"
+      permission   = "admin"
       name         = "Jacob Richards"
       email        = "jacob.richards@hmcts.net"
       org          = "CGI"
       reason       = "Support of service"
       added_by     = "david.pearce@Justice.gov.uk"
-      review_after = "2025-04-10"
+      review_after = "2025-08-30"
+    },
+    {
+      github_user  = "Dylan-365"
+      permission   = "admin"
+      name         = "Dylan Horman"
+      email        = "Dylan.horman@hmcts.net"
+      org          = "CGI"
+      reason       = "Support of service"
+      added_by     = "david.pearce@Justice.gov.uk"
+      review_after = "2025-08-30"
     },
   ]
 }
