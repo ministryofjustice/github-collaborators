1e6b36297e908471b346872e6acd2125e4266c09
Ross Jones <ross@register-dynamics.co.uk>
2024-12-03 10:54:28 +0000

    Add Becca

--- terraform/hmpps-community-accommodation-tier-2-bail-prototypes.tf
+++ terraform/hmpps-community-accommodation-tier-2-bail-prototypes.tf
@@ -31,6 +31,16 @@ module "hmpps-community-accommodation-tier-2-bail-prototypes" {
       reason       = "CAS 2 Bail Alpha Development"
       added_by     = "ross.jones@digital.justice.gov.uk"
       review_after = "2025-06-30"
+    },
+    {
+      github_user  = "beccagorton"
+      permission   = "push"
+      name         = "Becca Gorton"
+      email        = "becca.gorton@guest.vivace.tech"
+      org          = "TPXImpact"
+      reason       = "CAS 2 Bail Alpha Development"
+      added_by     = "ross.jones@digital.justice.gov.uk"
+      review_after = "2025-06-30"
     }
   ]
 }
