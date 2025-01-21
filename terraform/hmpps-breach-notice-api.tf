42fcd7e121d7e1ff1b481897172ef49bc0ea868a
Marcus Aspin <maspin@unilink.com>
2025-01-21 09:31:32 +0000

    Grant access for Peter Wilson to the Breach Notice repos

---
+++ terraform/hmpps-breach-notice-api.tf
@@ -0,0 +1,16 @@
+module "hmpps-breach-notice-api" {
+  source     = "./modules/repository-collaborators"
+  repository = "hmpps-breach-notice-api"
+  collaborators = [
+    {
+      github_user  = "peter-bcl"
+      permission   = "push"
+      name         = "Peter Wilson"
+      email        = "pwilson@unilink.com"
+      org          = "Unilink"
+      reason       = "To enable Unilink to continue supplying development and testing services to HMPPS"
+      added_by     = "marcus.aspin@digital.justice.gov.uk"
+      review_after = "2025-06-01"
+    },
+  ]
+}
